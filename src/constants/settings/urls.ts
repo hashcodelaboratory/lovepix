5b392e106eca90f5f83f135706d8936b897e1557
Michal <michaldzurilla1@gmail.com>
2022-08-10 09:36:36 +0200

    home-page - navigation constants added

---
+++ src/constants/settings/urls.ts
@@ -0,0 +1,3 @@
+export const PROFILE = '/profile';
+export const ACCOUNT = '/account';
+export const DASHBOARD = '/dashboard';
--- src/navigation.ts
+++ src/navigation.ts
@@ -1,16 +1,21 @@
+import * as PagesUrls from "./constants/pages/urls";
+import * as PagesTitles from "./constants/pages/titles";
+import * as SettingsTitles from "./constants/settings/titles";
+import * as SettingsUrls from "./constants/settings/urls";
+
 const pages = [
   // {title: 'Configurator', link: '/configurator'},
-  {title: 'Gallery', link: '/gallery'},
-  {title: 'Materials', link: '/materials'},
+  {title: PagesTitles.GALLERY, link: PagesUrls.GALLERY},
+  {title: PagesTitles.MATERIALS, link: PagesUrls.MATERIALS},
   // {title: 'For partners', link: '/for-partners'},
-  {title: 'About us', link: '/about-us'}
+  {title: PagesTitles.ABOUT_US, link: PagesUrls.ABOUT_US}
 ];
 
 const settings = [
-  {title: 'Profile', link: '/profile'},
-  {title: 'Account', link: '/account'},
-  {title: 'Dashboard', link: '/dashboard'},
-  {title: 'Logout', link: ''}
+  {title: SettingsTitles.PROFILE, link: SettingsUrls.PROFILE},
+  {title: SettingsTitles.ACCOUNT, link: SettingsUrls.ACCOUNT},
+  {title: SettingsTitles.DASHBOARD, link: SettingsUrls.DASHBOARD},
+  {title: SettingsTitles.LOGOUT, link: ''}
 ];
 
 export { pages, settings }
