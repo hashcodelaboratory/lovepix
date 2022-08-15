2dc452c2746957848c99b6367bf0f6413e351321
Michal <michaldzurilla1@gmail.com>
2022-08-15 13:27:16 +0200

    home-page - upload-image - messages deconstruction

--- src/screens-content/home/components/upload-image.tsx
+++ src/screens-content/home/components/upload-image.tsx
@@ -5,16 +5,18 @@ import styles from '../home.module.scss';
 const UploadImage = () => {
     const { t } = useTranslation();
 
+    const { printPhoto, uploadPhoto, uploadPhotoSubcontent } = messages;
+
     return(
         <>
             <h1 className={styles.title}>
-                {String(t(messages.printPhoto))}
+                {String(t(printPhoto))}
             </h1>
             <button className={styles.uploadButton} >
-                {String(t(messages.uploadPhoto))}
+                {String(t(uploadPhoto))}
             </button>
             <p className={styles.uploadPhotoSubcontent}>
-                {String(t(messages.uploadPhotoSubcontent))}
+                {String(t(uploadPhotoSubcontent))}
             </p>
         </>
     )
