bac6280b410f60b795dd3d03a8604a51cb4d83d6
Michal <michaldzurilla1@gmail.com>
2022-08-15 13:38:22 +0200

    home-page - next/Image added

--- src/screens-content/home/components/steps.tsx
+++ src/screens-content/home/components/steps.tsx
@@ -1,13 +1,14 @@
 import styles from "../home.module.scss";
 import {useTranslation} from "next-i18next";
 import {STEPS} from "../utils/steps";
+import Image from "next/image";
 
 const Steps = () => {
     const { t } = useTranslation();
 
     const columns = STEPS.map(({img, title}) =>
         <div key={img} className={styles.step}>
-            <img className={styles.stepImage} src={img} alt={title} />
+            <Image className={styles.stepImage} src={img} alt={title} width={200} height={100} />
             <div>{String(t(title))}</div>
         </div>
     )
