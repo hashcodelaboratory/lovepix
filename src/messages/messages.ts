ad007a09adba99b5e5610a7a2b7f1b6033f8b42b
Michal <michaldzurilla1@gmail.com>
2022-08-15 13:01:34 +0200

    home-page - steps section added

--- src/messages/messages.ts
+++ src/messages/messages.ts
@@ -1,5 +1,9 @@
 export const messages = {
   printPhoto: "printPhoto",
   uploadPhoto: "uploadPhoto",
-  uploadPhotoSubcontent: "uploadPhotoSubcontent"
+  uploadPhotoSubcontent: "uploadPhotoSubcontent",
+  uploadPhotoStepOne: "uploadPhotoStepOne",
+  uploadPhotoStepTwo: "uploadPhotoStepTwo",
+  uploadPhotoStepThree: "uploadPhotoStepThree",
+  uploadPhotoStepFour: "uploadPhotoStepFour",
 }
--- src/screens-content/home/main.tsx
+++ src/screens-content/home/main.tsx
@@ -1,10 +1,12 @@
 import UploadImagePart from "./components/upload-image-part";
 import {Container} from "@mui/material";
+import StepsPart from "./components/steps-part";
 
 const Main = () => {
     return(
         <Container>
             <UploadImagePart />
+            <StepsPart />
         </Container>
     )
 }
