e464efa737b82c21f0ce7b4c2e0e21dbb28e318d
Michal <michaldzurilla1@gmail.com>
2022-08-16 09:15:09 +0200

    home-page - responsive grid added

--- src/screens-content/home/components/materials.tsx
+++ src/screens-content/home/components/materials.tsx
@@ -2,21 +2,22 @@ import styles from "../home.module.scss";
 import {MATERIALS} from "../utils/materials";
 import Image from "next/image";
 import {useTranslation} from "next-i18next";
+import {Grid} from "@mui/material";
 
 const Materials = () => {
 
     const { t } = useTranslation();
 
     const columns = MATERIALS.map(({ img, title, text}) =>
-        <div key={title}>
+        <Grid key={title}>
             <Image className={styles.itemImage} src={img} width={300} height={250} />
             <div className={styles.materialsTitle}>{String(t(title))}</div>
             <div className={styles.materialsText}>{String(t(text))}</div>
-        </div>
+        </Grid>
     );
 
     return(
-        <div className={styles.horizontalContainer}>{columns}</div>
+        <Grid container className={styles.horizontalContainer}>{columns}</Grid>
     )
 }
 
--- src/screens-content/home/components/steps.tsx
+++ src/screens-content/home/components/steps.tsx
@@ -2,21 +2,22 @@ import styles from "../home.module.scss";
 import {useTranslation} from "next-i18next";
 import {STEPS} from "../utils/steps";
 import Image from "next/image";
+import {Grid} from "@mui/material";
 
 const Steps = () => {
     const { t } = useTranslation();
 
     const columns = STEPS.map(({img, title}) =>
-        <div key={img} className={styles.horizontalItem}>
+        <Grid key={img} className={styles.horizontalItem}>
             <Image className={styles.itemImage} src={img} alt={title} width={200} height={100} />
             <div className={styles.itemTitle}>{String(t(title))}</div>
-        </div>
+        </Grid>
     )
 
     return(
-        <div className={styles.horizontalContainer}>
+        <Grid container className={styles.horizontalContainer}>
             {columns}
-        </div>
+        </Grid>
     )
 }
 
