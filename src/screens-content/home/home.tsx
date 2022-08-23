b082cfffa27c928161b4f9ca0b6c03ca857ffdcb
Michal <michaldzurilla1@gmail.com>
2022-08-23 15:02:46 +0200

    #14 - dummy post test

--- src/screens-content/home/home.tsx
+++ src/screens-content/home/home.tsx
@@ -5,6 +5,18 @@ import Materials from "./components/materials/materials";
 import Gallery from "./components/gallery/gallery";
 
 const HomeLayout = () => {
+
+    // fetch('api/entry', {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         title: "title",
+    //         slug: "test",
+    //         body: "body"
+    //     })
+    // }).then(a => a.json()).then(b => console.log(b))
+    //
+    // fetch('/api/entries').then(a => a.json()).then(b => console.log(b))
+
     return(
         <Container>
             <UploadImage />
