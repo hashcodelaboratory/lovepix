e4ed2129a0706ee8d1ade7b59b083af491139d2c
Michal <michaldzurilla1@gmail.com>
2022-08-26 14:48:18 +0200

    #16 - content - sidebar translation added

--- src/screens-content/dashboard/components/sidebar/footer.tsx
+++ src/screens-content/dashboard/components/sidebar/footer.tsx
@@ -1,15 +1,22 @@
 import styles from '../../dashboard.module.scss'
 import Link from "next/link";
 import * as PagesUrls from "../../../../constants/pages/urls";
+import {useTranslation} from "next-i18next";
+import {messages} from "../../../../messages/messages";
 
-const SidebarFooter = () => (
-    <div className={styles.sidebarFooter}>
-        <Link href={PagesUrls.HOME}>
-            <button className={styles.sidebarFooterButton}>
-                Link to Home
-            </button>
-        </Link>
-    </div>
-)
+const SidebarFooter = () => {
+
+    const { t } = useTranslation();
+
+    return (
+        <div className={styles.sidebarFooter}>
+            <Link href={PagesUrls.HOME}>
+                <button className={styles.sidebarFooterButton}>
+                    {String(t(messages.home))}
+                </button>
+            </Link>
+        </div>
+    )
+}
 
 export default SidebarFooter
