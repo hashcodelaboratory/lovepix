9f7249c94094e1ed2c51868cb4d8b737ac484f90
Michal <michaldzurilla1@gmail.com>
2022-09-12 17:00:45 +0200

    #20 - cart - shopping-cart page added

--- src/app-bar/responsive-app-bar.tsx
+++ src/app-bar/responsive-app-bar.tsx
@@ -15,6 +15,8 @@ import Link from "next/link";
 import {pages, settings} from "../navigation";
 import {useTranslation} from "next-i18next";
 import { v4 as uuidv4 } from 'uuid';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import * as PagesUrls from "../constants/pages/urls";
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
@@ -127,7 +129,12 @@ const ResponsiveAppBar = () => {
             ))}
           </Box>
 
-          <Box sx={{flexGrow: 0}}>
+          <Box sx={{flexGrow: 0, display: 'flex'}}>
+              <Link href={PagesUrls.SHOPPING_CART} >
+                  <ShoppingCartIcon
+                      sx={{my: 2, color: 'white', display: 'block', marginRight: 2, cursor: 'pointer' }}
+                  />
+              </Link>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg"/>
--- src/constants/pages/urls.ts
+++ src/constants/pages/urls.ts
@@ -3,3 +3,4 @@ export const GALLERY = '/gallery';
 export const MATERIALS = '/materials';
 export const ABOUT_US = '/about-us';
 export const FOR_PARTNERS = '/for-partners';
+export const SHOPPING_CART = '/shopping-cart';
