7fa20e3a4aa45f5f3995a72168ca07617c73af59
Dominik Secka <secka.dominik@gmail.com>
2022-09-12 17:55:18 +0200

    #15 [image-container] key error fixed

--- src/screens-content/home/components/upload-image/image-container.tsx
+++ src/screens-content/home/components/upload-image/image-container.tsx
@@ -4,13 +4,16 @@ import {Grid} from "@mui/material";
 import ImageCard from "./image-card";
 import {Masonry} from "@mui/lab";
 import DropzoneContainer from "./dropzone/dropzone-container";
+import {v4 as uuidv4} from "uuid";
 
 const ImageContainer = () => (
   <Grid sm={12} textAlign={TextAlign.RIGHT} style={{marginTop: '64px'}}>
     <Masonry columns={3} spacing={4}>
       <DropzoneContainer/>
       {WALLER_IMAGE_LIST.map(({sourceUrl, title}) => (
+        <div key={uuidv4()}>
         <ImageCard sourceUrl={sourceUrl} title={title}/>
+        </div>
       ))}
       <button style={{ height: '200px', borderRadius: '0.5rem' }}>
         Gallery
