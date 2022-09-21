a0a642fdfecdf4fa09840d289e5a89fdd5495b30
Michal <michaldzurilla1@gmail.com>
2022-09-21 14:32:29 +0200

    #37 - order flow - translations added

--- src/screens-content/shopping-cart/components/summary/cart/cart.tsx
+++ src/screens-content/shopping-cart/components/summary/cart/cart.tsx
@@ -3,26 +3,39 @@ import {useContext} from "react";
 import AppContext from "../../../../../app-context/app-context";
 import Button from "@mui/material/Button";
 import Image from "next/image";
+import {TextField} from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import {useTranslation} from "next-i18next";
+import {messages} from "../../../../../messages/messages";
 
 const Cart = () => {
-    const { state: { image: { url } } } = useContext(AppContext);
+    const { state: { image: { url, size } } } = useContext(AppContext);
+
+    const { t } = useTranslation();
 
     const items =
-        <div className={styles.cartRow}>
-            <Image alt={url} src={url ?? ''} width={80} height={80} layout="fixed" />
-            <div>Konfiguracia</div>
-            <div>+ 1 -</div>
-            <div>17.99 eur</div>
-            <div>x</div>
-        </div>
+        <>
+            <div className={styles.cartRow}>
+                <Image alt={url} src={url ?? ''} width={80} height={80} layout="fixed" />
+                <div>Konfiguracia</div>
+                <div className={styles.qtyContainer}>
+                    <Button>-</Button>
+                    <TextField className={styles.qtyField} value={1} />
+                    <Button>+</Button>
+                </div>
+                <div>17.99 eur</div>
+                <Button><CloseIcon color="error" /></Button>
+            </div>
+            <hr/>
+        </>
 
     return (
         <div className={styles.cartContainer}>
-            <h1>Shoping cart</h1>
-            <p className={styles.itemsSize}>3 items</p>
+            <h1>{String(t(messages.shoppingCart))}</h1>
+            <p className={styles.itemsSize}>{size} {String(t(messages.items))}</p>
             <hr />
             {items}
-            <Button className={styles.backButton}>Back to shop</Button>
+            <Button className={styles.backButton}>{String(t(messages.backToShop))}</Button>
         </div>
     )
 }
