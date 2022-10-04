0ed7b6487b0383b5a9699f67ed3db3bd85ca4a35
Michal <michaldzurilla1@gmail.com>
2022-10-04 14:34:30 +0200

    #37 - order flow - empty-cart home redirect added

--- src/screens-content/shopping-cart/components/empty-cart/empty-cart.tsx
+++ src/screens-content/shopping-cart/components/empty-cart/empty-cart.tsx
@@ -1,19 +1,27 @@
 import {ShoppingCart} from "@mui/icons-material";
 import styles from '../../shopping-cart.module.scss';
-import {Container} from "@mui/material";
+import {Button, Container} from "@mui/material";
 import {useTranslation} from "next-i18next";
 import {messages} from "../../../../messages/messages";
+import {useRouter} from "next/router";
 
 
 const EmptyCart = () => {
 
     const { t } = useTranslation();
 
+    const router = useRouter();
+
+    const redirect = () => {
+        router.push("/");
+    }
+
     return (
         <Container >
             <div className={styles.emptyCartContainer}>
                 <ShoppingCart className={styles.icon} />
                 <p className={styles.title}>{String(t(messages.emptyCart))}</p>
+                <Button onClick={redirect}>{String(t(messages.backToShop))}</Button>
             </div>
         </Container>
     )
