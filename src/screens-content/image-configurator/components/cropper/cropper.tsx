95b54a90bce62b85c34c70f52e7d6b40085f85b9
Marek Micuda <marekmicuda1@gmail.com>
2022-10-25 22:45:44 +0200

    [Update]: Connecting context to configurator

--- src/screens-content/image-configurator/components/cropper/cropper.tsx
+++ src/screens-content/image-configurator/components/cropper/cropper.tsx
@@ -1,9 +1,38 @@
-import styles from '../../image-configurator-layout.module.scss';
+import styles from "../../image-configurator-layout.module.scss";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import { useContext, useRef } from "react";
+import AppContext from "app-context/app-context";
 
-const Cropper = () => {
-  return(
-      <div className={styles.cropper} />
-  )
-}
+const CropperComponent = () => {
+  const cropperRef = useRef<HTMLImageElement>(null);
 
-export default Cropper
+  const onCrop = () => {
+    const imageElement: any = cropperRef?.current;
+    const cropper: any = imageElement?.cropper;
+    console.log(cropper.getCroppedCanvas().toDataURL());
+  };
+
+  const {
+    state: { image },
+  } = useContext(AppContext);
+
+  console.log(image.url);
+
+  if (!image?.url) {
+    return null;
+  }
+  return (
+    <Cropper
+      src={image?.url}
+      style={{ height: 400, width: "100%" }}
+      // Cropper.js options
+      initialAspectRatio={16 / 9}
+      guides={false}
+      crop={onCrop}
+      ref={cropperRef}
+    />
+  );
+};
+
+export default CropperComponent;
--- src/screens-content/image-configurator/image-configurator-layout.tsx
+++ src/screens-content/image-configurator/image-configurator-layout.tsx
@@ -1,17 +1,17 @@
 import Cropper from "./components/cropper/cropper";
 import Sidebar from "./components/sidebar/sidebar";
-import styles from './image-configurator-layout.module.scss';
-import {Container} from "@mui/system";
+import styles from "./image-configurator-layout.module.scss";
+import { Container } from "@mui/system";
 
 const ImageConfiguratorLayout = () => {
-  return(
-      <Container>
-        <div className={styles.container}>
-          <Cropper />
-          <Sidebar />
-        </div>
-      </Container>
-  )
-}
+  return (
+    <Container>
+      <div className={styles.container}>
+        <Cropper />
+        <Sidebar />
+      </div>
+    </Container>
+  );
+};
 
-export default ImageConfiguratorLayout
+export default ImageConfiguratorLayout;
