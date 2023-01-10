618dcc42af521e402360bb38696222b345f1136f
Michal <michaldzurilla1@gmail.com>
2023-01-10 10:36:50 +0100

    #61 - dashboard - import fix

--- src/screens-content/dashboard/api/orders/useOrders.ts
+++ src/screens-content/dashboard/api/orders/useOrders.ts
@@ -2,8 +2,8 @@ import {useQuery, UseQueryResult} from "react-query";
 import {database} from "../../../../../utils/firebase/config";
 import {collection, getDocs} from "@firebase/firestore";
 import {Collections} from "../../../../../utils/firebase/enums";
-import {Order} from "./utils/types";
 import {ORDERS_KEY} from "./utils/keys";
+import {Order} from "../../../../common/types/order";
 
 const getOrders = async (): Promise<Order[]> => {
     const querySnapshot = await getDocs(collection(database, Collections.ORDERS));
