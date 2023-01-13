45989ec2a7a3c7bb50e187db9868cdbfa71ce0f9
Marek Micuda <marek@tryadvocate.com>
2023-01-13 09:09:25 +0100

    #62 - gallery - routing by gallery id

--- src/screens-content/home/components/upload-image/image-container.tsx
+++ src/screens-content/home/components/upload-image/image-container.tsx
@@ -1,25 +1,43 @@
-import {WALLER_IMAGE_LIST} from "../../utils/image-upload";
-import {TextAlign} from "../../enums/enums";
-import {Grid} from "@mui/material";
+import { WALLER_IMAGE_LIST } from "../../utils/image-upload";
+import { TextAlign } from "../../enums/enums";
+import { Grid } from "@mui/material";
 import ImageCard from "./image-card";
-import {Masonry} from "@mui/lab";
+import { Masonry } from "@mui/lab";
 import DropzoneContainer from "./dropzone/dropzone-container";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/router";
+import { CONFIGURATOR } from "constants/pages/urls";
 
-const ImageContainer = () => (
-  <Grid sm={12} textAlign={TextAlign.RIGHT} style={{marginTop: '64px'}}>
-    <Masonry columns={3} spacing={4}>
-      <DropzoneContainer/>
-      {WALLER_IMAGE_LIST.map(({sourceUrl, title}) => (
-        <div key={uuidv4()}>
-            <ImageCard sourceUrl={sourceUrl} title={title}/>
-        </div>
-      ))}
-      <button style={{ height: '200px', borderRadius: '0.5rem' }}>
-        Gallery
-      </button>
-    </Masonry>
-  </Grid>
-)
+const ImageContainer = () => {
+  const router = useRouter();
 
-export default ImageContainer
+  const handleConfiguration = (id: string) =>
+    router.push({
+      pathname: `/en${CONFIGURATOR}`,
+      query: { gallery: id },
+    });
+
+  return (
+    <Grid sm={12} textAlign={TextAlign.RIGHT} style={{ marginTop: "64px" }}>
+      <Masonry columns={3} spacing={4}>
+        <DropzoneContainer />
+        {WALLER_IMAGE_LIST.map(({ sourceUrl, title, id }) => {
+          return (
+            <div
+              style={{ cursor: "pointer" }}
+              key={uuidv4()}
+              onClick={() => handleConfiguration(id)}
+            >
+              <ImageCard sourceUrl={sourceUrl} title={title} />
+            </div>
+          );
+        })}
+        <button style={{ height: "200px", borderRadius: "0.5rem" }}>
+          Gallery
+        </button>
+      </Masonry>
+    </Grid>
+  );
+};
+
+export default ImageContainer;
--- src/screens-content/image-configurator/components/cropper/cropper.tsx
+++ src/screens-content/image-configurator/components/cropper/cropper.tsx
@@ -1,6 +1,6 @@
 import Cropper from "react-cropper";
 import "cropperjs/dist/cropper.css";
-import React, {useCallback, useContext, useEffect, useRef, useState} from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import AppContext from "app-context/app-context";
 import {
   dimensionsByHeight,
@@ -9,12 +9,44 @@ import {
 } from "screens-content/home/utils/configuration";
 import DropzoneContainer from "screens-content/home/components/upload-image/dropzone/dropzone-container";
 import ImageConfiguratorContext from "../../image-configurator-context/image-configurator-context";
+import { useRouter } from "next/router";
+import { WALLER_IMAGE_LIST } from "screens-content/home/utils/image-upload";
+import { ImageStatus } from "app-context/enums";
+import { useCreateOrder } from "screens-content/home/api/order/useCreateOrder";
 
 const CropperComponent = () => {
-  const { state: { image, dimensionId } } = useContext(AppContext);
-  const { stateAction: { setImage } } = useContext(ImageConfiguratorContext);
+  const {
+    state: { image, dimensionId },
+  } = useContext(AppContext);
+  const {
+    stateAction: { setImage },
+  } = useContext(ImageConfiguratorContext);
+
+  const { mutate: createOrder } = useCreateOrder();
 
   const cropperRef = useRef<any>(null);
+
+  const router = useRouter();
+
+  const galleryId = router?.query?.gallery as unknown as string;
+
+  useEffect(() => {
+    const source = WALLER_IMAGE_LIST.find((item) => item.id === galleryId);
+
+    const data = {
+      url: source?.sourceUrl,
+      status: ImageStatus.UPLOADED,
+      size: 1,
+      name: source?.title,
+    };
+
+    const date = Date.now();
+
+    console.log(data);
+
+    setImage(data as any);
+    createOrder({ image: data, date });
+  }, [createOrder, galleryId, setImage]);
 
   const onCrop = () => {
     const imageElement: any = cropperRef?.current;
