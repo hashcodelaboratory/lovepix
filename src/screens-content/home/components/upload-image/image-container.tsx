b958db29c40b818e60551f209eb18cc26d39a67e
Marek Micuda <marek@tryadvocate.com>
2023-01-23 11:45:32 +0100

    #62 - gallery - Refactoring

--- src/screens-content/home/components/upload-image/image-container.tsx
+++ src/screens-content/home/components/upload-image/image-container.tsx
@@ -6,52 +6,26 @@ import { Masonry } from "@mui/lab";
 import DropzoneContainer from "./dropzone/dropzone-container";
 import { v4 as uuidv4 } from "uuid";
 import { useRouter } from "next/router";
-import { CONFIGURATOR } from "constants/pages/urls";
 import { configurationsTable } from "../../../../../database.config";
 import { useLiveQuery } from "dexie-react-hooks";
+import { CONF } from "common/indexed-db/enums/storeNames";
+import { handleDB } from "./utils";
 
 const ImageContainer = () => {
   const router = useRouter();
 
-  const data = useLiveQuery(() => configurationsTable.get("conf"), []);
-
-  const handleConfiguration = async (sourceUrl: string) => () => {
-    const res = await fetch(sourceUrl ?? "");
-
-    const file = await res.blob();
-
-    const fr = new FileReader();
-
-    fr.readAsDataURL(file);
-
-    fr.onload = () => {
-      const dataPayload = {
-        origin: fr.result as string,
-        image: undefined,
-        dimensionId: undefined,
-        material: undefined,
-      };
-
-      data
-        ? configurationsTable.update("conf", dataPayload)
-        : configurationsTable.add(dataPayload, "conf");
-    };
-
-    router.push({
-      pathname: CONFIGURATOR,
-    });
-  };
+  const data = useLiveQuery(() => configurationsTable.get(CONF), []);
 
   return (
     <Grid sm={12} textAlign={TextAlign.RIGHT} style={{ marginTop: "64px" }}>
       <Masonry columns={3} spacing={4}>
         <DropzoneContainer />
-        {WALLER_IMAGE_LIST.map(({ sourceUrl, title, id }) => {
+        {WALLER_IMAGE_LIST.map(({ sourceUrl, title }) => {
           return (
             <div
               style={{ cursor: "pointer" }}
               key={uuidv4()}
-              onClick={handleConfiguration(sourceUrl)}
+              onClick={handleDB(sourceUrl, data, configurationsTable, router)}
             >
               <ImageCard sourceUrl={sourceUrl} title={title} />
             </div>
--- src/screens-content/home/components/upload-image/utils.ts
+++ src/screens-content/home/components/upload-image/utils.ts
@@ -1,5 +1,46 @@
-const imageSource = (sourceUrl: string) => `${sourceUrl}?w=248&fit=crop&auto=format`
+import { CONF } from "common/indexed-db/enums/storeNames";
+import { CONFIGURATOR } from "constants/pages/urls";
+import { IndexableType, Table } from "dexie";
+import { NextRouter } from "next/router";
 
-const imageSourceSet = (sourceUrl: string) => `${sourceUrl}?w=248&fit=crop&auto=format&dpr=2 2x`
+const imageSource = (sourceUrl: string) =>
+  `${sourceUrl}?w=248&fit=crop&auto=format`;
 
-export { imageSource, imageSourceSet }
+const imageSourceSet = (sourceUrl: string) =>
+  `${sourceUrl}?w=248&fit=crop&auto=format&dpr=2 2x`;
+
+const handleDB =
+  (
+    sourceUrl: string,
+    data: unknown,
+    configurationsTable: Table<any, IndexableType>,
+    router: NextRouter
+  ) =>
+  async () => {
+    const res = await fetch(sourceUrl ?? "");
+
+    const file = await res.blob();
+
+    const fr = new FileReader();
+
+    fr.readAsDataURL(file);
+
+    fr.onload = () => {
+      const dataPayload = {
+        origin: fr.result as string,
+        image: undefined,
+        dimensionId: undefined,
+        material: undefined,
+      };
+
+      data
+        ? configurationsTable.update(CONF, dataPayload)
+        : configurationsTable.add(dataPayload, CONF);
+    };
+
+    router.push({
+      pathname: CONFIGURATOR,
+    });
+  };
+
+export { imageSource, imageSourceSet, handleDB };
