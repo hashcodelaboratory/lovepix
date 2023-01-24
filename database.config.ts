4981c0f2a4f7eafdd5942bf1d25a6ea95c7d2edd
Michal <michaldzurilla1@gmail.com>
2023-01-24 10:46:04 +0100

    #56 - database - name fix

--- database.config.ts
+++ database.config.ts
@@ -1,7 +1,7 @@
 import Dexie from "dexie";
 import {StoreNames} from "./src/common/indexed-db/enums/storeNames";
 
-const database = new Dexie("database");
+const database = new Dexie("waller");
 database.version(1).stores({
     configurations: '',
     order: ''
