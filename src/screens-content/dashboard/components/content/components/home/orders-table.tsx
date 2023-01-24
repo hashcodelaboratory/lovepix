7815a0eb4cf0dcf6d7a9b6cf4016f048e8b95717
Michal <michaldzurilla1@gmail.com>
2023-01-24 10:44:51 +0100

    #56 - dashboard - accordion added

--- src/screens-content/dashboard/components/content/components/home/orders-table.tsx
+++ src/screens-content/dashboard/components/content/components/home/orders-table.tsx
@@ -11,6 +11,8 @@ import { useQueryClient } from "react-query";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { ORDERS_COLUMNS } from "./utils/ordersColumns";
 import { removeOrders } from "./utils/removeOrders";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const OrdersTable = () => {
   const { state: { orders } } = useContext(DashboardContext);
@@ -31,8 +33,8 @@ const OrdersTable = () => {
       totalPrice: totalPrice ?? "",
       delivery: t(delivery),
       payment: t(payment) ?? "",
-      origin: shoppingCart?.images?.map(({ origin }) => origin) ?? '-',
-      edited: shoppingCart?.images?.map(({ url }) => url) ?? '-',
+      origin: shoppingCart?.images?.map(({ origin }) => origin) ?? "-",
+      edited: shoppingCart?.images?.map(({ url }) => url) ?? "-",
       pdf: shoppingCart?.images?.map((image) => ({ image, id, pdf })),
     }
   ));
@@ -60,27 +62,36 @@ const OrdersTable = () => {
   const buttonText = `(${selectedRows.length}) ${String(t(messages.removeAll))}`;
 
   return (
-    <>
-      <h1>{String(t(messages.orders))}</h1>
-      <Box sx={{ height: 400, width: "100%", marginBottom: 12 }}>
-        <DataGrid
-          className={styles.contentTable}
-          rows={data ?? []}
-          columns={ORDERS_COLUMNS}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-          checkboxSelection
-          disableSelectionOnClick
-          selectionModel={selectionModel}
-          onSelectionModelChange={selectionChanged}
-          components={{ Toolbar: GridToolbar }}
-        />
-        <button className={styles.removeButton} onClick={removeData} disabled={!selectedRows.length}>
-          {buttonText}
-          <DeleteIcon sx={{ marginLeft: 1 }} />
-        </button>
-      </Box>
-    </>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <h1>{String(t(messages.orders))}</h1>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Box sx={{ height: 400, width: "100%", marginBottom: 12 }}>
+          <DataGrid
+            className={styles.contentTable}
+            rows={data ?? []}
+            columns={ORDERS_COLUMNS}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+            disableSelectionOnClick
+            selectionModel={selectionModel}
+            onSelectionModelChange={selectionChanged}
+            components={{ Toolbar: GridToolbar }}
+            autoHeight
+          />
+          <button className={styles.removeButton} onClick={removeData} disabled={!selectedRows.length}>
+            {buttonText}
+            <DeleteIcon sx={{ marginLeft: 1 }} />
+          </button>
+        </Box>
+      </AccordionDetails>
+    </Accordion>
   );
 };
 
--- src/screens-content/dashboard/components/content/components/home/storage-table.tsx
+++ src/screens-content/dashboard/components/content/components/home/storage-table.tsx
@@ -1,81 +1,89 @@
-import Box from '@mui/material/Box';
-import {DataGrid, GridCallbackDetails, GridSelectionModel} from '@mui/x-data-grid';
-import styles from '../../../../dashboard.module.scss'
-import {useContext, useState} from "react";
+import { DataGrid, GridCallbackDetails, GridSelectionModel } from "@mui/x-data-grid";
+import styles from "../../../../dashboard.module.scss";
+import { useContext, useState } from "react";
 import DashboardContext from "../../../../context/dashboard-context";
-import {UPLOADED_IMAGES_COLUMNS} from "./utils/columns";
-import {messages} from "../../../../../../messages/messages";
-import {SNACKBAR_OPTIONS_ERROR, SNACKBAR_OPTIONS_SUCCESS} from "../../../../../../snackbar/config";
-import {useSnackbar} from "notistack";
-import {useTranslation} from "next-i18next";
-import {useQueryClient} from "react-query";
-import DeleteIcon from '@mui/icons-material/Delete';
-import {removeUploadedImages} from "./utils/removeUploadedImages";
+import { UPLOADED_IMAGES_COLUMNS } from "./utils/columns";
+import { messages } from "../../../../../../messages/messages";
+import { SNACKBAR_OPTIONS_ERROR, SNACKBAR_OPTIONS_SUCCESS } from "../../../../../../snackbar/config";
+import { useSnackbar } from "notistack";
+import { useTranslation } from "next-i18next";
+import { useQueryClient } from "react-query";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { removeUploadedImages } from "./utils/removeUploadedImages";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const StorageTable = () => {
-    const { state: { uploadedImages } } = useContext(DashboardContext);
+  const { state: { uploadedImages } } = useContext(DashboardContext);
 
-    const { enqueueSnackbar } = useSnackbar();
+  const { enqueueSnackbar } = useSnackbar();
 
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const [selectedRows, setSelectedRows] = useState<string[]>([]);
-    const [selectionModel, setSelectionModel] = useState<GridSelectionModel>([]);
+  const [selectedRows, setSelectedRows] = useState<string[]>([]);
+  const [selectionModel, setSelectionModel] = useState<GridSelectionModel>([]);
 
-    const data = uploadedImages.map(({ bucket, name, fullPath }, index) => (
-        {
-            id: index + 1,
-            bucket: bucket,
-            order: name,
-        }
-    ));
-
-    const reset = () => {
-        setSelectionModel([]);
-        setSelectedRows([]);
+  const data = uploadedImages.map(({ bucket, name, fullPath }, index) => (
+    {
+      id: index + 1,
+      bucket: bucket,
+      order: name,
     }
+  ));
 
-    const removeData = () => {
-        const result = removeUploadedImages(selectedRows, queryClient);
-        if (result === "") {
-            enqueueSnackbar(String(t(messages.filesRemoved)), SNACKBAR_OPTIONS_SUCCESS);
-            reset();
-        } else {
-            enqueueSnackbar(result, SNACKBAR_OPTIONS_ERROR);
-        }
+  const reset = () => {
+    setSelectionModel([]);
+    setSelectedRows([]);
+  };
+
+  const removeData = () => {
+    const result = removeUploadedImages(selectedRows, queryClient);
+    if (result === "") {
+      enqueueSnackbar(String(t(messages.filesRemoved)), SNACKBAR_OPTIONS_SUCCESS);
+      reset();
+    } else {
+      enqueueSnackbar(result, SNACKBAR_OPTIONS_ERROR);
     }
+  };
 
-    const selectionChanged = (selectionModel: GridSelectionModel, details: GridCallbackDetails) => {
-        setSelectionModel(selectionModel);
-        setSelectedRows(selectionModel.map((item, index) => data[index].order));
-    }
+  const selectionChanged = (selectionModel: GridSelectionModel, details: GridCallbackDetails) => {
+    setSelectionModel(selectionModel);
+    setSelectedRows(selectionModel.map((item, index) => data[index].order));
+  };
 
-    const buttonText = `(${selectedRows.length}) ${String(t(messages.removeAll))}`;
+  const buttonText = `(${selectedRows.length}) ${String(t(messages.removeAll))}`;
 
-    return (
-        <>
-            <h1>{String(t(messages.storage))}</h1>
-            <Box sx={{height: 400, width: '100%'}}>
-                <DataGrid
-                    className={styles.contentTable}
-                    rows={data}
-                    columns={UPLOADED_IMAGES_COLUMNS}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                    disableSelectionOnClick
-                    selectionModel={selectionModel}
-                    onSelectionModelChange={selectionChanged}
-                />
-                <button className={styles.removeButton} onClick={removeData} disabled={selectedRows.length === 0}>
-                    {buttonText}
-                    <DeleteIcon sx={{marginLeft: 1}}/>
-                </button>
-            </Box>
-        </>
-    )
-}
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <h1>{String(t(messages.storage))}</h1>
+      </AccordionSummary>
+      <AccordionDetails>
+        <DataGrid
+          className={styles.contentTable}
+          rows={data}
+          columns={UPLOADED_IMAGES_COLUMNS}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+          selectionModel={selectionModel}
+          onSelectionModelChange={selectionChanged}
+          autoHeight
+        />
+        <button className={styles.removeButton} onClick={removeData} disabled={selectedRows.length === 0}>
+          {buttonText}
+          <DeleteIcon sx={{ marginLeft: 1 }} />
+        </button>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
 
-export default StorageTable
+export default StorageTable;
