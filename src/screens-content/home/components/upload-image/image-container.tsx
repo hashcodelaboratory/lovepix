92fb3385f4aa9461e2af4de8ad99f9dfd3bbffe0
Michal <michaldzurilla1@gmail.com>
2023-03-22 17:23:25 +0100

    #97 - home page - responsive layout added

--- src/screens-content/home/components/upload-image/image-container.tsx
+++ src/screens-content/home/components/upload-image/image-container.tsx
@@ -9,6 +9,7 @@ import { useRouter } from "next/router";
 import { configurationsTable } from "../../../../../database.config";
 import { handleDB } from "./utils";
 import { Configuration } from "../../../../common/types/configuration";
+import styles from '../../home.module.scss'
 
 type ImageContainerProps = {
   configuration: Configuration;
@@ -17,26 +18,29 @@ type ImageContainerProps = {
 const ImageContainer = ({ configuration }: ImageContainerProps) => {
   const router = useRouter();
 
+  const IMAGE_LIST = WALLER_IMAGE_LIST.map(({ sourceUrl, title }) => (
+      <div
+        style={{ cursor: "pointer" }}
+        key={uuidv4()}
+        onClick={handleDB(sourceUrl, configuration, configurationsTable, router)}
+      >
+        <ImageCard sourceUrl={sourceUrl} title={title} />
+      </div>
+    ),
+  );
+
   return (
-    <Grid sm={12} textAlign={TextAlign.RIGHT} style={{ marginTop: "64px" }}>
-      <Masonry columns={3} spacing={4}>
-        <DropzoneContainer configuration={configuration} />
-        {WALLER_IMAGE_LIST.map(({ sourceUrl, title }) => {
-          return (
-            <div
-              style={{ cursor: "pointer" }}
-              key={uuidv4()}
-              onClick={handleDB(sourceUrl, configuration, configurationsTable, router)}
-            >
-              <ImageCard sourceUrl={sourceUrl} title={title} />
-            </div>
-          );
-        })}
-        <button style={{ height: "200px", borderRadius: "0.5rem" }}>
-          Gallery
-        </button>
-      </Masonry>
-    </Grid>
+    <div className={styles.home}>
+      <DropzoneContainer configuration={configuration} />
+      <Grid sm={12} textAlign={TextAlign.RIGHT} style={{ marginTop: "64px" }}>
+        <Masonry columns={3} spacing={2}>
+          {IMAGE_LIST}
+          <button className={styles.galleryButton}>
+            Gallery
+          </button>
+        </Masonry>
+      </Grid>
+    </div>
   );
 };
 
