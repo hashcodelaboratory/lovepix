8c29adeba20d16ca9c00d49e60145076a1a254a1
Michal <michaldzurilla1@gmail.com>
2023-03-23 16:30:10 +0100

    #111 - cropper page - sidebar updated

--- src/screens-content/image-configurator/components/sidebar/components/header/header.tsx
+++ src/screens-content/image-configurator/components/sidebar/components/header/header.tsx
@@ -1,17 +1,22 @@
-import styles from '../../../../image-configurator-layout.module.scss'
+import styles from "../../../../image-configurator-layout.module.scss";
 
 type Props = {
   icon: JSX.Element
   title: string
+  success: boolean;
 }
 
-const Header = ({ icon, title }: Props) => {
+const Header = ({ icon, title, success }: Props) => {
   return (
     <div className={styles.header}>
       {icon}
-      <h3 className={styles.headerTitle}>{title}</h3>
+      <h3 className={styles.headerTitle}
+          style={{ color: success ? "green" : "black" }}
+      >
+        {title}
+      </h3>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
