7b134a221eb13b5abe0e40186dc0ee62e5ec1e85
Michal <michaldzurilla1@gmail.com>
2023-03-31 13:37:19 +0200

    #100 - cart page - removeImage async removed

--- src/screens-content/shopping-cart/components/summary/components/delivery/utils/utils.ts
+++ src/screens-content/shopping-cart/components/summary/components/delivery/utils/utils.ts
@@ -2,7 +2,7 @@ import { orderTable } from "../../../../../../../../database.config";
 import { ORDER_TABLE_KEY } from "../../../../../../../common/indexed-db/hooks/keys";
 import { Image } from "../../../../../../../common/types/order";
 
-export const removeImage = async (url?: string, images?: Image[]) => {
+export const removeImage = (url?: string, images?: Image[]) => {
   if (images?.length === 1) {
     orderTable.clear();
   } else {
