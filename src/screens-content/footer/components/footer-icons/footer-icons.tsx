5ace0db11fa3234e702eb3a880d6e571f9247e94
Michal <michaldzurilla1@gmail.com>
2023-06-01 12:14:33 +0200

    #100 - home page - footer updated

--- src/screens-content/footer/components/footer-icons/footer-icons.tsx
+++ src/screens-content/footer/components/footer-icons/footer-icons.tsx
@@ -1,25 +1,83 @@
-import FacebookIcon from "@mui/icons-material/Facebook";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import TwitterIcon from "@mui/icons-material/Twitter";
+import lovepixIcon from "../../../../assets/logo_gray.svg";
+import facebookIcon from "../../../../assets/facebook.svg";
+import twitterIcon from "../../../../assets/twitter.svg";
+import instagramIcon from "../../../../assets/instagram.svg";
 import styles from "../../footer.module.scss";
+import { Container } from "@mui/material";
+import Image from "next/image";
+import { ImageLayout } from "../../../home/enums/enums";
+import { useTranslation } from "react-i18next";
+import { messages } from "../../../../messages/messages";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const FooterIcons = (): JSX.Element => {
+  const { t } = useTranslation();
+
   return (
-    <>
+    <Container>
+      <hr />
       <div className={styles.footerIconsRow}>
-        <FacebookIcon className={styles.footerIcon} sx={{ color: "#3B579D" }} />
-        <InstagramIcon className={styles.footerIcon} sx={{ color: "#22578A" }} />
-        <YouTubeIcon className={styles.footerIcon} sx={{ color: "#C31B18" }} />
-        <TwitterIcon className={styles.footerIcon} sx={{ color: "#46AAE4" }} />
+        <Image
+          src={lovepixIcon}
+          layout={ImageLayout.FIXED}
+          width={22}
+          height={22}
+        />
+        <p className={styles.footerIconsText}><b>{t(messages.partners)}:</b></p>
+        <p className={styles.footerIconsText}>mojkalendar.sk</p>
+        <p className={styles.footerIconsText}>hobbyfotograf.sk</p>
+        <p className={styles.footerIconsText}>hashlab.sk</p>
       </div>
-      <div className={styles.footerContainer}>
-        <p className={styles.footerContainerTitle}>Copyright 2023 WESTech, spol. s r.o., Stará Vajnorská 17,
-          Bratislava</p>
-        <p className={styles.footerContainerTitle}>Všetky práva vyhradené.</p>
-        <p className={styles.footerContainerText}>Technické riešenie © 2023 Hashlab s.r.o.</p>
+      <hr />
+      <div className={styles.footerBottomContainer}>
+        <div className={styles.footerBottomContainerRow}>
+          <p className={styles.footerBottomContainerRowText}>{t(messages.changeLanguage)}</p>
+          <KeyboardArrowDownIcon sx={{ color: "gray", mr: 3 }} />
+          <div className={styles.footerBottomIcon}>
+            <Image
+              src={facebookIcon}
+              layout={ImageLayout.FIXED}
+              width={22}
+              height={22}
+
+            />
+          </div>
+          <div className={styles.footerBottomIcon}>
+            <Image
+              src={instagramIcon}
+              layout={ImageLayout.FIXED}
+              width={22}
+              height={22}
+            />
+          </div>
+          <div className={styles.footerBottomIcon}>
+            <Image
+              src={twitterIcon}
+              layout={ImageLayout.FIXED}
+              width={22}
+              height={22}
+              className={styles.footerBottomIcon}
+            />
+          </div>
+        </div>
+        <div className={styles.footerBottomContainerRow}>
+          <p className={styles.footerBottomContainerRowText} style={{ marginRight: 36 }}>
+            Copyright © 2023 Lovepix. Všetky práva vyhradené.
+          </p>
+          <p className={styles.footerBottomContainerRowTextLink}>
+            Všeobecné obchodné podmienky
+          </p>
+          <p className={styles.footerDivider}>/</p>
+          <p className={styles.footerBottomContainerRowTextLink}>
+            Zásady ochrany osobných údajov
+          </p>
+          <p className={styles.footerDivider}>/</p>
+          <p className={styles.footerBottomContainerRowTextLink}>
+            Cookies
+          </p>
+        </div>
       </div>
-    </>
+    </Container>
   );
 };
 
--- src/screens-content/footer/components/footer-links/footer-links.tsx
+++ src/screens-content/footer/components/footer-links/footer-links.tsx
@@ -1,30 +1,49 @@
 import styles from "../../footer.module.scss";
 import Container from "@mui/material/Container";
+import { messages } from "../../../../messages/messages";
+import { useTranslation } from "react-i18next";
+import { useCategories } from "../../../../common/api/use-categories";
+import { FOR_PARTNERS } from "../../../../constants/pages/titles";
 
 const FooterLinks = (): JSX.Element => {
+  const { data: categories } = useCategories();
+
   return (
     <Container>
       <div className={styles.footerRow}>
-        <FooterColumn />
-        <FooterColumn />
-        <FooterColumn />
-        <FooterColumn />
-        <FooterColumn />
+        <FooterColumn
+          title={messages.service}
+          links={[messages.ourContacts, messages.satisfaction, messages.possibilities, messages.creatingTime, messages.complaint]}
+        />
+        <FooterColumn
+          title={messages.fromPhoto}
+          links={[messages.canvasPhoto, messages.acrylPhoto, messages.dibondPhoto]}
+        />
+        <FooterColumn
+          title={messages.gallery}
+          links={categories?.map(({ name }) => name) ?? []}
+        />
+        <FooterColumn
+          title={"Lovepix"}
+          links={[messages.materials, messages.story, FOR_PARTNERS, messages.download, messages.blog]}
+        />
       </div>
     </Container>
   );
 };
 
-const FooterColumn = (): JSX.Element => {
+type FooterColumnType = {
+  title: string;
+  links: string[];
+}
+
+const FooterColumn = ({ title, links }: FooterColumnType): JSX.Element => {
+  const { t } = useTranslation();
+
   return (
-    <div>
-      <h3 className={styles.footerTitle}>Category title</h3>
-      <p className={styles.footerText}>Subtitle</p>
-      <p className={styles.footerText}>Subtitle</p>
-      <p className={styles.footerText}>Subtitle</p>
-      <p className={styles.footerText}>Subtitle</p>
-      <p className={styles.footerText}>Subtitle</p>
-      <p className={styles.footerText}>Subtitle</p>
+    <div className={styles.footerColumn}>
+      <h3 className={styles.footerTitle}>{t(title)}</h3>
+      {links.map((link) => <p key={link} className={styles.footerText}>{t(link)}</p>)}
     </div>
   );
 };
