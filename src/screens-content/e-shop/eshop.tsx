8b6842f50df852bf1dfd8a89a11886ad3a9ee0f3
DavidBednarik <davidbednarik07@gmail.com>
2023-06-12 12:50:22 +0200

    eshop page init

---
+++ src/screens-content/e-shop/eshop.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material'
+import React from 'react'
+import styles from './eshop.module.scss'
+
+const EshopLayout = () => {
+  return (
+    <Container>
+      <div className={styles.eshopLayout}>Eshop</div>
+    </Container>
+  )
+}
+
+export default EshopLayout
