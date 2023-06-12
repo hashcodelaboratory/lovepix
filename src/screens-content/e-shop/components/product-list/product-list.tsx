4e7f93aa61a5e777d36637a2864aefa364e6c867
DavidBednarik <davidbednarik07@gmail.com>
2023-06-12 14:46:50 +0200

    get product data from firestore

--- src/screens-content/e-shop/components/product-list/product-list.tsx
+++ src/screens-content/e-shop/components/product-list/product-list.tsx
@@ -1,18 +1,16 @@
+import { ProductsType, useProducts } from 'common/api/use-products'
 import React from 'react'
 import Product from '../product/product'
 import styles from './product-list.module.scss'
 
 const ProductList = () => {
-  return (
-    <div className={styles.productsContainer}>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-    </div>
-  )
+  const { data: products } = useProducts()
+
+  const productList = products?.map((products: ProductsType) => (
+    <Product key={products.id} product={{ ...products }} />
+  ))
+
+  return <div className={styles.productsContainer}>{productList}</div>
 }
 
 export default ProductList
--- src/screens-content/e-shop/components/product/product.tsx
+++ src/screens-content/e-shop/components/product/product.tsx
@@ -1,8 +1,33 @@
+import { Button } from '@mui/material'
+import { ProductsType } from 'common/api/use-products'
+import Image from 'next/image'
 import React from 'react'
+import { ImageLayout } from 'screens-content/home/enums/enums'
 import styles from './product.module.scss'
 
-const Product = () => {
-  return <div className={styles.productCart}>Product</div>
+type ProductContent = {
+  product: ProductsType
+}
+
+const Product = ({ product }: ProductContent) => {
+  const { id, title, price, image } = product
+  return (
+    <div className={styles.productCart}>
+      <Image
+        src={image}
+        layout={ImageLayout.INTRINSIC}
+        width={200}
+        height={200}
+        alt='image'
+        className={styles.image}
+      />
+      <span className={styles.title}>{title}</span>
+      <span className={styles.price}>{price.toFixed(2)} €</span>
+      <Button variant='outlined' className={styles.addToCart}>
+        Pridať do košíka
+      </Button>
+    </div>
+  )
 }
 
 export default Product
