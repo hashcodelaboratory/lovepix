b676fdcd9ba97af32ff535d827570a66cec04774
DavidBednarik <davidbednarik07@gmail.com>
2023-06-14 11:59:56 +0200

    update image background color

--- src/screens-content/e-shop/components/product-detail/product-detail.tsx
+++ src/screens-content/e-shop/components/product-detail/product-detail.tsx
@@ -24,6 +24,7 @@ const ProductDetail = ({ id }: ProductID) => {
           height={400}
           alt='image'
           className={styles.image}
+          loading='lazy'
         />
         <div className={styles.productInfo}>
           <span className={styles.title}>{title}</span>
