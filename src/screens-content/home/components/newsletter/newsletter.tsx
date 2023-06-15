07d2643126a6d567695e7297bcb3ef96e87d3113
Michal <michaldzurilla1@gmail.com>
2023-06-15 14:29:51 +0200

    #212- home page - Newsletter component UI updated

--- src/screens-content/home/components/newsletter/newsletter.tsx
+++ src/screens-content/home/components/newsletter/newsletter.tsx
@@ -1,41 +1,55 @@
-import styles from "./newsletter.module.scss"
+import styles from "./newsletter.module.scss";
 import { useTranslation } from "react-i18next";
 import { messages } from "../../../../messages/messages";
 import Container from "@mui/material/Container";
 import { TextField } from "@mui/material";
 
 const Newsletter = (): JSX.Element => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
-    <Container>
-      <div className={styles.container}>
-        <div className={styles.border}>
-          <p className={styles.title}>{String(t(messages.newsletterLogin))}</p>
-          <p className={styles.subtitle}>{String(t(messages.newsletterGet))}</p>
-          <div className={styles.row}>
-            <p className={styles.textFieldText}>{String(t(messages.putMail))}</p>
-            <p className={styles.textFieldTextSecondary}>{String(t(messages.logout))}</p>
+    <div className={styles.layout}>
+      <Container>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <p className={styles.title}>{String(t(messages.newsletterLogin))}</p>
+            <p className={styles.subtitle}>{String(t(messages.newsletterGet))}</p>
+            <div className={styles.row}>
+              <p className={styles.textFieldText}>{String(t(messages.putMail))}</p>
+              <p className={styles.textFieldTextSecondary}>{String(t(messages.logout))}</p>
+            </div>
+            <div className={styles.row}>
+              <TextField
+                placeholder={String(t(messages.yourMail))}
+                fullWidth
+                variant="standard"
+                size="small"
+                sx={{
+                  width: "70%",
+                  backgroundColor: "white",
+                  padding: 0.8,
+                  marginRight: 1,
+                  borderRadius: 2,
+                  '& .MuiInput-root': {
+                    '&:before, :after, :hover:not(.Mui-disabled):before': {
+                      borderBottom: 0,
+                    },
+                  },
+                }}
+              />
+              <button className={styles.button}>
+                {String(t(messages.login))}
+              </button>
+            </div>
+            <p className={styles.note}>
+              {String(t(messages.newsletter))}
+              <p className={styles.noteLink}>{String(t(messages.here))}</p>
+            </p>
           </div>
-          <TextField
-            placeholder={String(t(messages.yourMail))}
-            fullWidth
-            variant="outlined"
-            size="small"
-            className={styles.formField}
-            sx={{ width: "80%" }}
-          />
-          <button className={styles.button}>
-            {String(t(messages.login))}
-          </button>
-          <p className={styles.note}>
-            {String(t(messages.newsletter))}
-            <p className={styles.noteLink}>{String(t(messages.here))}</p>
-          </p>
         </div>
-      </div>
-    </Container>
-  )
-}
+      </Container>
+    </div>
+  );
+};
 
 export default Newsletter;
