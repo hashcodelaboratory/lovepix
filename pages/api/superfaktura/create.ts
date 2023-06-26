65aa636e09f7afb6ff98cb31b56181856c96ebd5
DavidBednarik <davidbednarik07@gmail.com>
2023-06-26 11:04:40 +0200

    added superfaktura to next api

---
+++ pages/api/superfaktura/create.ts
@@ -0,0 +1,54 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+
+const API_URL_CREATE = 'https://moja.superfaktura.sk/invoices/create'
+const BAD_REQUEST_ERROR_MESSAGE = 'Bad request!'
+
+const create = async (req: NextApiRequest, res: NextApiResponse<unknown>) => {
+  try {
+    if (req.method !== 'POST') {
+      res.status(400).json({
+        error: BAD_REQUEST_ERROR_MESSAGE,
+      })
+
+      return
+    }
+
+    if (!req.body) {
+      res.status(400).json({
+        error: BAD_REQUEST_ERROR_MESSAGE,
+      })
+
+      return
+    }
+
+    const parsedBody = JSON.parse(req.body)
+
+    if (!parsedBody) {
+      res.status(400).json({
+        error: BAD_REQUEST_ERROR_MESSAGE,
+      })
+
+      return
+    }
+
+    const config = {
+      headers: {
+        Authorization: `SFAPI email=${process.env.AUTH_EMAIL}&apikey=${process.env.AUTH_API_KEY}&company_id=${process.env.AUTH_COMPANY_ID}&module=${process.env.AUTH_MODULE}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+    }
+
+    const result = await axios.post(
+      API_URL_CREATE,
+      'data=' + JSON.stringify(parsedBody),
+      config
+    )
+
+    res.status(201).json(result.data as unknown)
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message })
+  }
+}
+
+export default create
--- src/common/api/superfaktura.ts
+++ src/common/api/superfaktura.ts
@@ -1,11 +1,8 @@
 import { SFInvoice } from 'common/types/superfaktura'
 
 export const addInvoice = async (invoice: SFInvoice) => {
-  return await fetch(
-    'https://us-central1-waller-development.cloudfunctions.net/createInvoice',
-    {
-      method: 'POST',
-      body: JSON.stringify(invoice),
-    }
-  )
+  return await fetch('/api/superfaktura/create', {
+    method: 'POST',
+    body: JSON.stringify(invoice),
+  })
 }
