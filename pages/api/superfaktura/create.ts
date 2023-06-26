873f20028c262943e229bb561fd16737b2d3368a
DavidBednarik <davidbednarik07@gmail.com>
2023-06-26 13:40:36 +0200

    change env template key

--- pages/api/superfaktura/create.ts
+++ pages/api/superfaktura/create.ts
@@ -34,7 +34,7 @@ const create = async (req: NextApiRequest, res: NextApiResponse<unknown>) => {
 
     const config = {
       headers: {
-        Authorization: `SFAPI email=${process.env.AUTH_EMAIL}&apikey=${process.env.AUTH_API_KEY}&company_id=${process.env.AUTH_COMPANY_ID}&module=${process.env.AUTH_MODULE}`,
+        Authorization: `SFAPI email=${process.env.NEXT_PUBLIC_AUTH_EMAIL}&apikey=${process.env.NEXT_PUBLIC_AUTH_API_KEY}&company_id=${process.env.NEXT_PUBLIC_AUTH_COMPANY_ID}&module=${process.env.NEXT_PUBLIC_AUTH_MODULE}`,
         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
       },
     }
