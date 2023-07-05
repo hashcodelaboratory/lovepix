061dc973bd9ebf432a9e6af62652b0345f4903e5
Zuzana Hrklova <xhrklo00@vutbr.cz>
2023-07-05 19:31:07 +0200

    favicon update

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,6 +7,7 @@ import {SnackbarProvider} from 'notistack'
 import {QueryClient, QueryClientProvider} from 'react-query'
 import {useState} from 'react'
 import {ReactQueryDevtools} from 'react-query/devtools'
+import Head from 'next/head'
 
 import 'vanilla-cookieconsent';
 import 'vanilla-cookieconsent/dist/cookieconsent.css';
@@ -17,6 +18,17 @@ function MyApp({Component, pageProps}: AppProps) {
 
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <QueryClientProvider client={queryClient}>
         <SnackbarProvider
           anchorOrigin={{
