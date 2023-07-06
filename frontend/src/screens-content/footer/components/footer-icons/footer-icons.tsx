9f8f44ce08253b744b0b8e66c68c924574e5d7ed
hydro63 <115730210+hydro63@users.noreply.github.com>
2023-07-06 14:55:02 +0200

    #208 [Homepage] [Footer] Links - changed <a> to <Link>
    Corrected tags from <a> to <Link>, corrected the addresses of the links

--- frontend/src/screens-content/footer/components/footer-icons/footer-icons.tsx
+++ frontend/src/screens-content/footer/components/footer-icons/footer-icons.tsx
@@ -3,7 +3,7 @@ import facebookIcon from "../../../../assets/facebook.svg";
 import twitterIcon from "../../../../assets/twitter.svg";
 import instagramIcon from "../../../../assets/instagram.svg";
 import styles from "../../footer.module.scss";
-import { Container } from "@mui/material";
+import { Container, Link } from "@mui/material";
 import Image from "next/image";
 import { ImageLayout } from "../../../home/enums/enums";
 import { useTranslation } from "react-i18next";
@@ -11,7 +11,7 @@ import { messages } from "../../../../messages/messages";
 
 const FooterIcons = (): JSX.Element => {
   const { t } = useTranslation();
-
+  // external href in <Link> does not work without 2 leading slashes or 'https://' 
   return (
     <Container>
       <hr />
@@ -24,9 +24,9 @@ const FooterIcons = (): JSX.Element => {
           alt=""
         />
         <p className={styles.footerIconsText}><b>{t(messages.partners)}:</b></p>
-        <a className={styles.footerIconsText} href="//mojkalendar.sk" target="_blank" rel="noreferrer">mojkalendar.sk</a>
-        <a className={styles.footerIconsText} href="//odfotma.sk" target="_blank" rel="noreferrer">odfotma.sk</a>
-        <a className={styles.footerIconsText} href="//hashlab.com" target="_blank" rel="noreferrer">hashlab.com</a>
+        <Link className={styles.footerIconsText} href="https://www.mojkalendar.sk" target="_blank" rel="noreferrer">mojkalendar.sk</Link>
+        <Link className={styles.footerIconsText} href="https://www.odfotma.sk" target="_blank" rel="noreferrer">odfotma.sk</Link>
+        <Link className={styles.footerIconsText} href="https://www.hashlab.com" target="_blank" rel="noreferrer">hashlab.com</Link>
       </div>
       <hr />
       <div className={styles.footerBottomContainer}>
@@ -64,17 +64,17 @@ const FooterIcons = (): JSX.Element => {
           <p className={styles.footerBottomContainerRowText} style={{ marginRight: 36 }}>
             Copyright © 2023 Lovepix. Všetky práva vyhradené.
           </p>
-          <a className={styles.footerBottomContainerRowTextLink} href="vseobecne-obchodne-podmienky" target="_blank">
+          <Link className={styles.footerBottomContainerRowTextLink} href="/vseobecne-obchodne-podmienky" target="_blank">
             Všeobecné obchodné podmienky
-          </a>
+          </Link>
           <p className={styles.footerDivider}>/</p>
-          <a className={styles.footerBottomContainerRowTextLink} href="zasady-ochrany-osobnych-udajov" target="_blank">
+          <Link className={styles.footerBottomContainerRowTextLink} href="/zasady-ochrany-osobnych-udajov" target="_blank">
             Zásady ochrany osobných údajov
-          </a>
+          </Link>
           <p className={styles.footerDivider}>/</p>
-          <a className={styles.footerBottomContainerRowTextLink} href="cookies" target="_blank">
+          <Link className={styles.footerBottomContainerRowTextLink} href="/cookies" target="_blank">
             Cookies
-          </a>
+          </Link>
         </div>
       </div>
     </Container>
