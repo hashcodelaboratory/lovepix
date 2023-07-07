7cda344ec887e9f55609c013802a423f916e8678
Michal <michaldzurilla1@gmail.com>
2023-07-07 16:45:19 +0200

    shopping-cart - price dimensions fix

--- frontend/src/screens-content/image-configurator/components/sidebar/components/price/price.tsx
+++ frontend/src/screens-content/image-configurator/components/sidebar/components/price/price.tsx
@@ -1,30 +1,33 @@
-import styles from "../../../../image-configurator-layout.module.scss";
-import { getPrice } from "./utils/generator";
-import { materials } from "../../../../../home/utils/configuration";
-import { DIMENSIONS } from "../../../../../../common/configuration/dimensions/dimensions";
-import { Configuration } from "../../../../../../common/types/configuration";
+import styles from '../../../../image-configurator-layout.module.scss'
+import { getPrice } from './utils/generator'
+import { materials } from '../../../../../home/utils/configuration'
+import { DIMENSIONS } from '../../../../../../common/configuration/dimensions/dimensions'
+import { Configuration } from '../../../../../../common/types/configuration'
+import { splitDimension } from '../../../../../../common/utils/split-dimension'
 
 type PriceProps = {
-  configuration: Configuration;
+  configuration: Configuration
 }
 
 const Price = ({ configuration }: PriceProps) => {
-  const { width, height } = DIMENSIONS.find(
-    (dim) => dim.id === configuration?.dimensionId,
-  ) ?? { width: 0, height: 0 };
+  const { width, height } = splitDimension(configuration?.dimensionId) ?? {
+    width: 0,
+    height: 0,
+  }
 
   const computedPrice =
     width > 0 && height > 0
       ? getPrice(
-        width,
-        height,
-        materials.find((material) => material.id === configuration?.material)
-          ?.name,
-      )
-      : "-";
+          width,
+          height,
+          materials.find((material) => material.id === configuration?.material)
+            ?.name
+        )
+      : '-'
 
-  const noTaxPrice = computedPrice !== "-" ? Number(computedPrice * 0.8).toFixed(2) : "--";
-  const price = computedPrice !== "-" ? Number(computedPrice).toFixed(2) : "--";
+  const noTaxPrice =
+    computedPrice !== '-' ? Number(computedPrice * 0.8).toFixed(2) : '--'
+  const price = computedPrice !== '-' ? Number(computedPrice).toFixed(2) : '--'
 
   return (
     <div className={styles.containerPadding}>
@@ -32,16 +35,14 @@ const Price = ({ configuration }: PriceProps) => {
         <h4>
           <b>Cena</b>
         </h4>
-        <p className={styles.priceNoTax}>
-          {noTaxPrice} € bez DPH
-        </p>
+        <p className={styles.priceNoTax}>{noTaxPrice} € bez DPH</p>
         <h4>
           <b>{price} € </b>
         </h4>
       </div>
       <hr />
     </div>
-  );
-};
+  )
+}
 
-export default Price;
+export default Price
