e5c75afe71b0e8c95881fd0eb477bd996e062311
Michal <michaldzurilla1@gmail.com>
2023-07-07 15:31:04 +0200

    gallery - api get gallery item detail

--- frontend/src/screens-content/image-configurator/components/sidebar/components/dimension/components/dimension-content/dimension-content.tsx
+++ frontend/src/screens-content/image-configurator/components/sidebar/components/dimension/components/dimension-content/dimension-content.tsx
@@ -1,65 +1,63 @@
-import Box from "@mui/material/Box";
-import { Tabs, Tab } from "@mui/material";
-import { SyntheticEvent, useEffect, useState } from "react";
-import { a11yProps } from "./utils/utils";
-import TabPanel from "./components/tab-panel";
-import { useTranslation } from "next-i18next";
-import { messages } from "../../../../../../../../messages/messages";
-import TabPanelBox from "./components/tab-panel-box";
+import Box from '@mui/material/Box'
+import { Tabs, Tab } from '@mui/material'
+import { SyntheticEvent, useEffect, useState } from 'react'
+import { a11yProps } from './utils/utils'
+import TabPanel from './components/tab-panel'
+import { useTranslation } from 'next-i18next'
+import { messages } from '../../../../../../../../messages/messages'
+import TabPanelBox from './components/tab-panel-box'
 import {
   dimensionsByHeight,
   dimensionsBySquare,
   dimensionsByWidth,
-} from "screens-content/home/utils/configuration";
-import { configurationsTable } from "../../../../../../../../../database.config";
-import { CONFIGURATION_TABLE_KEY } from "../../../../../../../../common/indexed-db/hooks/keys";
-import { Configuration } from "../../../../../../../../common/types/configuration";
+} from 'screens-content/home/utils/configuration'
+import { configurationsTable } from '../../../../../../../../../database.config'
+import { CONFIGURATION_TABLE_KEY } from '../../../../../../../../common/indexed-db/hooks/keys'
+import { Configuration } from '../../../../../../../../common/types/configuration'
 
 type DimensionContentProps = {
-  configuration: Configuration;
+  configuration: Configuration
 }
 
 const DimensionContent = ({ configuration }: DimensionContentProps) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
-  const [value, setValue] = useState(0);
+  const dimensionsSquare = dimensionsBySquare
+  const dimensionsWidth = dimensionsByWidth
+  const dimensionsHeight = dimensionsByHeight
+
+  const [value, setValue] = useState(0)
 
   useEffect(() => {
-    if (
-      dimensionsBySquare.find((dim) => dim.id === configuration?.dimensionId)
-    ) {
-      setValue(2);
+    if (dimensionsSquare.find((dim) => dim.id === configuration?.dimensionId)) {
+      setValue(2)
     }
-    if (
-      dimensionsByWidth.find((dim) => dim.id === configuration?.dimensionId)
-    ) {
-      setValue(0);
+    if (dimensionsWidth.find((dim) => dim.id === configuration?.dimensionId)) {
+      setValue(0)
     }
-    if (
-      dimensionsByHeight.find((dim) => dim.id === configuration?.dimensionId)
-    ) {
-      setValue(1);
+    if (dimensionsHeight.find((dim) => dim.id === configuration?.dimensionId)) {
+      setValue(1)
     }
-  }, [configuration?.dimensionId]);
+  }, [configuration?.dimensionId])
 
   const handleChange = (event: SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+    setValue(newValue)
+  }
 
   const changeDimension = (id: string) => {
     configurationsTable.update(CONFIGURATION_TABLE_KEY, {
       dimensionId: id,
-    });
-  };
+    })
+  }
 
   return (
     <div>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs
           value={value}
           onChange={handleChange}
-          aria-label="basic tabs example"
-          variant="fullWidth"
+          aria-label='basic tabs example'
+          variant='fullWidth'
           visibleScrollbar
         >
           <Tab label={String(t(messages.byWidth))} {...a11yProps(0)} />
@@ -68,7 +66,7 @@ const DimensionContent = ({ configuration }: DimensionContentProps) => {
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        {dimensionsByWidth.map((dim) => (
+        {dimensionsWidth.map((dim) => (
           <TabPanelBox
             selected={dim.id === configuration?.dimensionId}
             x={dim.width}
@@ -79,7 +77,7 @@ const DimensionContent = ({ configuration }: DimensionContentProps) => {
         ))}
       </TabPanel>
       <TabPanel value={value} index={1}>
-        {dimensionsByHeight.map((dim) => (
+        {dimensionsHeight.map((dim) => (
           <TabPanelBox
             selected={dim.id === configuration?.dimensionId}
             x={dim.width}
@@ -91,7 +89,7 @@ const DimensionContent = ({ configuration }: DimensionContentProps) => {
         ))}
       </TabPanel>
       <TabPanel value={value} index={2}>
-        {dimensionsBySquare.map((dim) => (
+        {dimensionsSquare.map((dim) => (
           <TabPanelBox
             selected={dim.id === configuration?.dimensionId}
             x={dim.width}
@@ -103,7 +101,7 @@ const DimensionContent = ({ configuration }: DimensionContentProps) => {
         ))}
       </TabPanel>
     </div>
-  );
-};
+  )
+}
 
-export default DimensionContent;
+export default DimensionContent
