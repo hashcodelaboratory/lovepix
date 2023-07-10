2b25920000b95c89c142f5890f001a939e46b356
MatejCarnansky <matej.carnansky@gmail.com>
2023-07-10 17:47:45 +0200

    add prisma, docker and replica mongodb

---
+++ backend/src/todo/todo.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+
+@Injectable()
+export class TodoService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createTodoDto: CreateTodoDto) {
+    return this.prismaService.todo.create({
+      data: createTodoDto
+    })
+  }
+
+  findAll() {
+    return `This action returns all todo`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} todo`;
+  }
+
+  update(id: number, updateTodoDto: UpdateTodoDto) {
+    return `This action updates a #${id} todo`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} todo`;
+  }
+}
