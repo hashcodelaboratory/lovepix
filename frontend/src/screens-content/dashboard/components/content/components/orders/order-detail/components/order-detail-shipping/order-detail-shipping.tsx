bc7e9f05832e473d0c76c12a442c9abcab248ceb
Michal <michaldzurilla1@gmail.com>
2023-07-10 16:23:00 +0200

    dashboard - orders-info - info updated

--- frontend/src/screens-content/dashboard/components/content/components/orders/order-detail/components/order-detail-shipping/order-detail-shipping.tsx
+++ frontend/src/screens-content/dashboard/components/content/components/orders/order-detail/components/order-detail-shipping/order-detail-shipping.tsx
@@ -1,22 +1,29 @@
-import styles from "../../order-detail.module.scss";
-import { messages } from "../../../../../../../../../messages/messages";
-import { Box } from "@mui/material";
-import { Order } from "../../../../../../../../../common/types/order";
-import { useTranslation } from "next-i18next";
+import styles from '../../order-detail.module.scss'
+import { messages } from '../../../../../../../../../messages/messages'
+import { Box } from '@mui/material'
+import { Order } from '../../../../../../../../../common/types/order'
+import { useTranslation } from 'next-i18next'
+import { LocalShipping as LocalShippingIcon } from '@mui/icons-material'
 
 type Props = {
-  order?: Order;
+  order?: Order
 }
 
 const OrderDetailShipping = ({ order }: Props): JSX.Element => {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   return (
     <Box className={styles.box} style={{ width: 400 }}>
       <h4>{t(messages.shipping)}</h4>
-      <div style={{ fontSize: 12 }}>{order?.form?.address} {order?.form?.city} {order?.form?.postalCode}</div>
+      <div style={{ fontSize: 12 }}>
+        {order?.form?.address} {order?.form?.city} {order?.form?.postalCode}
+      </div>
+      <div className={styles.detailRow} style={{ marginTop: 20 }}>
+        <LocalShippingIcon className={styles.detailIcon} />
+        {t(order?.delivery ?? '')}
+      </div>
     </Box>
-  );
-};
+  )
+}
 
-export default OrderDetailShipping;
+export default OrderDetailShipping
