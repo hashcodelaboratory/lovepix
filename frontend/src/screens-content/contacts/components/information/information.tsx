d356120671eadcc3b279837b430fea9dbbc377a4
hydro63 <peter.hudak.63nn@gmail.com>
2023-07-13 10:03:37 +0200

    #207 [Contacts] Redesign site

--- frontend/src/screens-content/contacts/components/information/information.tsx
+++ frontend/src/screens-content/contacts/components/information/information.tsx
@@ -5,36 +5,38 @@ import { useTranslation } from "react-i18next";
 const Information = (): JSX.Element => {
 
   return (
-      <Container>
-        <InfoRow key="kontakt" title="Kontakt:" values={[[""]]}></InfoRow>
-        <InfoRow key="phone" title="Telefónny kontakt:" values={[["+421 947 904 580"]]}></InfoRow>
-        <InfoRow key="faktura" title="Fakturačné údaje:" values={[["Hashlab s.r.o","https://finstat.sk/52575420"]]}></InfoRow>
-        <InfoRow key="place" title="Odberné miesto:" values={[["Progrup s.r.o"],["Zvonárska 2886/30 A | 052 01 Spišská Nová Ves"]]}></InfoRow>
-      </Container>
+    <Container>
+      <div>
+        <div className={styles.infoTitleContainer}>
+          <h1 className={styles.infoTitle}>Kontakt</h1>
+          <p className={styles.infoComment}>Potrebujete poradiť? Nenašli ste čo ste hľadali? 
+            Potrebujete vyrobiť niečo na mieru? Radi vám s tým 
+            - pomôžeme. Ozvite sa nám!
+          </p>
+        </div>
+        <InfoRow title="Odberné miesto" values={["Progrup s.r.o.","Zvonárenská 2886/30 A","052 01 Spišská Nová Ves"]}></InfoRow>
+        <InfoRow title="Fakturačné údaje" values={["Hashlab s.r.o.","52575420","Obchodný register Mestského súdu Košice","oddiel: Sro, vložka č. 46939/V"]}></InfoRow>
+      </div>
+      
+      <h2 className={styles.infoSocialTitle}>Sociálne siete</h2>
+      <Link href="#" rel="noreferrer" target="_blank" className={styles.infoSocialLink}>Facebook</Link>
+      <Link href="#" rel="noreferrer" target="_blank" className={styles.infoSocialLink}>Instagram</Link>
+      <Link href="#" rel="noreferrer" target="_blank" className={styles.infoSocialLink}>Tiktok</Link>
+    </Container>
   );
 };
 
 type InfoRowType = {
   title: string;
-  values: string[][];
+  values: string[];
 }
 
 const InfoRow = ({title, values}: InfoRowType): JSX.Element => {
-  let ret = [];
-  for(let i=0;i<values.length;i++){
-    if(values[i].length === 1) ret.push(<p key={values[i][0]} className={styles.infoValueText}>{values[i][0]}</p>);
-    if(values[i].length === 2) ret.push(<Link key={values[i][0]} className={styles.infoValueLink} href={values[i][1]} target="_blank" rel="noreferrer">{values[i][0]}</Link>);
-  }
   return (
-    <Container>
     <div className={styles.infoRow}>
-      <p className={styles.infoTitle}>{title}</p>
-      <div>
-        {ret}
-      </div>
-      
+      <p className={styles.infoValueTitle}>{title}</p>
+      {values.map(value => <p className={styles.infoValue}>{value}</p>)}
     </div>
-    </Container>
   );
 };
 
--- frontend/src/screens-content/contacts/contacts.tsx
+++ frontend/src/screens-content/contacts/contacts.tsx
@@ -4,11 +4,29 @@ import styles from "./contacts.module.scss"
 import Map from "./components/map/map"
 const ContactsLayout = (): JSX.Element => {
   return (
-    <Container className={styles.mainContainer}>
-      <Information></Information>
-      <Map></Map>
-    </Container>
+    <Grid container spacing={6} className={styles.mainContainer} maxWidth="lg">
+      <Grid item xs={12} md={5}>
+          <Information/>
+      </Grid>
+
+      <Grid item xs={12} md={7} className={styles.mapContainer}>
+          <Map/>
+      </Grid>
+    </Grid>
   )
 }
 
 export default ContactsLayout;
+
+/*
+
+    <Grid container spacing={2}>
+      <Grid xs={12} md={6}>
+          <Information/>
+      </Grid>
+
+      <Grid xs={12} md={6}>
+          <Map/>
+      </Grid>
+    </Grid>
+*/
