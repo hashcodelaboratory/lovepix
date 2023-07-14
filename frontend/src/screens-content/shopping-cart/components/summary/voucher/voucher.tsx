8b7d8323cf072ab35973e517ee771d4b9533700f
Michal <michaldzurilla1@gmail.com>
2023-07-14 11:08:15 +0200

    #211 - voucher - button added

--- frontend/src/screens-content/shopping-cart/components/summary/voucher/voucher.tsx
+++ frontend/src/screens-content/shopping-cart/components/summary/voucher/voucher.tsx
@@ -1,25 +1,29 @@
-import styles from "../../../shopping-cart.module.scss";
-import { useTranslation } from "next-i18next";
-import { messages } from "../../../../../messages/messages";
-import { TextField } from "@mui/material";
+import styles from '../../../shopping-cart.module.scss'
+import { useTranslation } from 'next-i18next'
+import { messages } from '../../../../../messages/messages'
+import { TextField } from '@mui/material'
+import Button from '@mui/material/Button'
 
 const Voucher = () => {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   return (
     <div className={styles.cartContainer}>
       <div className={styles.cartTitleContainer}>
         <h3 className={styles.cartTitleText}>{String(t(messages.code))}</h3>
       </div>
-      <TextField
-        className={styles.voucherTextField}
-        label={String(t(messages.code))}
-        placeholder="WALLER22"
-        InputLabelProps={{ shrink: true }}
-        size="small"
-      />
+      <div className={styles.voucherRow}>
+        <TextField
+          className={styles.voucherTextField}
+          label={String(t(messages.code))}
+          placeholder='WALLER22'
+          InputLabelProps={{ shrink: true }}
+          size='small'
+        />
+        <Button variant='contained'>{String(t(messages.add))}</Button>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Voucher;
+export default Voucher
