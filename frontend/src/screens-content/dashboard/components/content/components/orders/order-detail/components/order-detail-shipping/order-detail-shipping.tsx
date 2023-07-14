a81f4656af3b9c752cfdc79baa39ac6a80c12b01
DavidBednarik <davidbednarik07@gmail.com>
2023-07-14 10:53:31 +0200

    added note to localization

--- frontend/src/screens-content/dashboard/components/content/components/orders/order-detail/components/order-detail-shipping/order-detail-shipping.tsx
+++ frontend/src/screens-content/dashboard/components/content/components/orders/order-detail/components/order-detail-shipping/order-detail-shipping.tsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material'
 import { Order } from '../../../../../../../../../common/types/order'
 import { useTranslation } from 'next-i18next'
 import { LocalShipping as LocalShippingIcon } from '@mui/icons-material'
+import { messages } from 'messages/messages'
 
 type Props = {
   order?: Order
@@ -14,18 +15,28 @@ const OrderDetailShipping = ({ order }: Props): JSX.Element => {
   const { ico, dic, company, address, city, postalCode, note } =
     order?.form ?? {}
 
+  const formular = [
+    { title: 'Ico', value: ico },
+    { title: 'DIC', value: dic },
+    { title: messages.company, value: company },
+    { title: messages.address, value: address },
+    { title: messages.city, value: city },
+    { title: messages.postalCode, value: postalCode },
+    { title: messages.note, value: note },
+  ]
+
+  const formList = formular.map((item, index) => (
+    <div key={index} style={{ fontSize: 12 }}>
+      <p>
+        {t(item.title)}: {item.value}
+      </p>
+    </div>
+  ))
+
   return (
     <Box className={styles.box} style={{ width: 400 }}>
       <h4>Fakturačná adresa</h4>
-      <div style={{ fontSize: 12 }}>
-        <p>ICO: {ico}</p>
-        <p>DIC: {dic}</p>
-        <p>COMPANY: {company}</p>
-        <p>{address}</p>
-        <p>{city} </p>
-        <p>{postalCode}</p>
-        <p>{note}</p>
-      </div>
+      {formList}
       <div className={styles.detailRow} style={{ marginTop: 20 }}>
         <LocalShippingIcon className={styles.detailIcon} />
         {t(order?.delivery ?? '')}
