152d9964ccf91642d22958c24bbf328a76971dbf
Michal <michaldzurilla1@gmail.com>
2023-07-17 10:43:59 +0200

    #211 - dashboard - side panel detail added

--- frontend/src/screens-content/dashboard/components/content/components/voucher/components/voucher-detail/voucher-detail.tsx
+++ frontend/src/screens-content/dashboard/components/content/components/voucher/components/voucher-detail/voucher-detail.tsx
@@ -4,8 +4,34 @@ import Button from '@mui/material/Button'
 import SettingsIcon from '@mui/icons-material/Settings'
 import HideSourceIcon from '@mui/icons-material/HideSource'
 import MultipleStopIcon from '@mui/icons-material/MultipleStop'
+import { useMemo, useState } from 'react'
+
+enum SidepanelEnum {
+  GENERAL = 'GENERAL',
+  STRICT = 'STRICT',
+  LIMIT = 'LIMIT',
+}
 
 const VoucherDetail = () => {
+  const [sidepanel, setSidepanel] = useState<SidepanelEnum>(
+    SidepanelEnum.GENERAL
+  )
+
+  const detailLayout = useMemo(() => {
+    switch (sidepanel) {
+      case SidepanelEnum.GENERAL:
+        return 'general'
+      case SidepanelEnum.STRICT:
+        return 'strict'
+      case SidepanelEnum.LIMIT:
+        return 'limit'
+    }
+  }, [sidepanel])
+
+  const changeSidepanel = (param: SidepanelEnum) => {
+    setSidepanel(param)
+  }
+
   return (
     <div className={styles.voucherDetailBox}>
       <div>
@@ -29,18 +55,32 @@ const VoucherDetail = () => {
         <p className={styles.voucherDetailInputText} style={{ marginTop: 8 }}>
           Údaje kupónu
         </p>
-        <div>
-          <div className={styles.voucherDetailListRow}>
-            <SettingsIcon sx={{ mr: 1 }} />
-            <p>Všeobecné</p>
+        <div className={styles.voucherDetailSidepanelRow}>
+          <div>
+            <div
+              className={styles.voucherDetailListRow}
+              onClick={() => changeSidepanel(SidepanelEnum.GENERAL)}
+            >
+              <SettingsIcon sx={{ mr: 1 }} />
+              <p>Všeobecné</p>
+            </div>
+            <div
+              className={styles.voucherDetailListRow}
+              onClick={() => changeSidepanel(SidepanelEnum.STRICT)}
+            >
+              <HideSourceIcon sx={{ mr: 1 }} />
+              <p>Obmedzenie použitia</p>
+            </div>
+            <div
+              className={styles.voucherDetailListRow}
+              onClick={() => changeSidepanel(SidepanelEnum.LIMIT)}
+            >
+              <MultipleStopIcon sx={{ mr: 1 }} />
+              <p>Limit využitia</p>
+            </div>
           </div>
-          <div className={styles.voucherDetailListRow}>
-            <HideSourceIcon sx={{ mr: 1 }} />
-            <p>Obmedzenie použitia</p>
-          </div>
-          <div className={styles.voucherDetailListRow}>
-            <MultipleStopIcon sx={{ mr: 1 }} />
-            <p>Limit využitia</p>
+          <div className={styles.voucherDetailSidepanelContent}>
+            {detailLayout}
           </div>
         </div>
       </div>
