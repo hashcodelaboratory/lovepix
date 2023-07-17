0be9ec3c6831ab35f29a8ac395c10181ca6258d8
DavidBednarik <davidbednarik07@gmail.com>
2023-07-17 14:23:56 +0200

    filter products

--- frontend/src/common/api/use-products.ts
+++ frontend/src/common/api/use-products.ts
@@ -2,6 +2,8 @@ import { useQuery, UseQueryResult } from 'react-query'
 import { collection, getDocs } from '@firebase/firestore'
 import { database } from '../firebase/config'
 import { Collections } from '../firebase/enums'
+import { query, where } from 'firebase/firestore'
+import { all } from 'axios'
 
 export const PRODUCT_KEY = 'PRODUCTS'
 
@@ -14,14 +16,25 @@ export type ProductsType = {
   count: number
 }
 
-const getProducts = async (): Promise<ProductsType[]> => {
-  const querySnapshot = await getDocs(
-    collection(database, Collections.PRODUCTS)
+const getProducts = async (
+  category?: string | null
+): Promise<ProductsType[]> => {
+  console.log(category)
+
+  const allProducts = collection(database, Collections.PRODUCTS)
+  const filteredProducts = query(
+    collection(database, Collections.PRODUCTS),
+    where('category', '==', category)
   )
+  const option = category !== null ? filteredProducts : allProducts
+  const querySnapshot = await getDocs(option)
+
   return querySnapshot.docs.map(
     (doc) => ({ id: doc.id, ...doc.data() } as ProductsType)
   )
 }
 
-export const useProducts = (): UseQueryResult<ProductsType[]> =>
-  useQuery([PRODUCT_KEY], () => getProducts())
+export const useProducts = (
+  category?: string | null
+): UseQueryResult<ProductsType[]> =>
+  useQuery([PRODUCT_KEY], () => getProducts(category))
--- frontend/src/screens-content/e-shop/components/product-list/product-list.tsx
+++ frontend/src/screens-content/e-shop/components/product-list/product-list.tsx
@@ -1,20 +1,42 @@
 import { ProductsType, useProducts } from 'common/api/use-products'
-import React, { useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductSkeleton from '../product-skeleton/product-skeleton'
 import Product from '../product/product'
 import styles from './product-list.module.scss'
 import { useCategoriesEshop } from 'common/api/use-categories-eshop'
 
 const ProductList = () => {
-  const { data: products, isLoading } = useProducts()
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
+  const { data: products, isLoading, refetch } = useProducts(selectedCategory)
   const { data: categories } = useCategoriesEshop()
 
   const productList = products?.map((products: ProductsType) => (
     <Product key={products.id} product={{ ...products }} />
   ))
 
+  console.log(selectedCategory)
+
+  useEffect(() => {
+    refetch()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedCategory])
+
+  const handleChange = (category: string) => () => {
+    setSelectedCategory(category)
+  }
+
   const categoriesList = categories?.map((item, index) => (
-    <div key={index}>{item.name}</div>
+    <div
+      key={index}
+      onClick={handleChange(item.name)}
+      className={
+        selectedCategory === item.name
+          ? styles.categoryActive
+          : styles.categoryItem
+      }
+    >
+      {item.name}
+    </div>
   ))
 
   return (
