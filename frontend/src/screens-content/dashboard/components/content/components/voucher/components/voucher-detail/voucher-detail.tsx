d4b573937a8f0789ddd3ecff76751705049f80b5
Michal <michaldzurilla1@gmail.com>
2023-07-17 10:29:31 +0200

    #211 - dashboard - side panel added

--- frontend/src/screens-content/dashboard/components/content/components/voucher/components/voucher-detail/voucher-detail.tsx
+++ frontend/src/screens-content/dashboard/components/content/components/voucher/components/voucher-detail/voucher-detail.tsx
@@ -1,6 +1,9 @@
 import styles from '../../../../../../dashboard.module.scss'
 import { TextField } from '@mui/material'
 import Button from '@mui/material/Button'
+import SettingsIcon from '@mui/icons-material/Settings'
+import HideSourceIcon from '@mui/icons-material/HideSource'
+import MultipleStopIcon from '@mui/icons-material/MultipleStop'
 
 const VoucherDetail = () => {
   return (
@@ -21,6 +24,25 @@ const VoucherDetail = () => {
           rows={3}
           placeholder='Popis kupónu'
         />
+      </div>
+      <div>
+        <p className={styles.voucherDetailInputText} style={{ marginTop: 8 }}>
+          Údaje kupónu
+        </p>
+        <div>
+          <div className={styles.voucherDetailListRow}>
+            <SettingsIcon sx={{ mr: 1 }} />
+            <p>Všeobecné</p>
+          </div>
+          <div className={styles.voucherDetailListRow}>
+            <HideSourceIcon sx={{ mr: 1 }} />
+            <p>Obmedzenie použitia</p>
+          </div>
+          <div className={styles.voucherDetailListRow}>
+            <MultipleStopIcon sx={{ mr: 1 }} />
+            <p>Limit využitia</p>
+          </div>
+        </div>
       </div>
     </div>
   )
