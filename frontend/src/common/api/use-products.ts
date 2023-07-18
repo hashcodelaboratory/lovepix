e6b0519720bb6602f6de8fa05a68dd9e5087c0da
DavidBednarik <davidbednarik07@gmail.com>
2023-07-18 20:20:00 +0200

    get products hook

--- frontend/src/common/api/use-products.ts
+++ frontend/src/common/api/use-products.ts
@@ -2,8 +2,6 @@ import { useQuery, UseQueryResult } from 'react-query'
 import { collection, getDocs } from '@firebase/firestore'
 import { database } from '../firebase/config'
 import { Collections } from '../firebase/enums'
-import { query, where } from 'firebase/firestore'
-import { all } from 'axios'
 
 export const PRODUCT_KEY = 'PRODUCTS'
 
@@ -16,25 +14,15 @@ export type ProductsType = {
   count: number
 }
 
-const getProducts = async (
-  category?: string | null
-): Promise<ProductsType[]> => {
-  console.log(category)
-
-  const allProducts = collection(database, Collections.PRODUCTS)
-  const filteredProducts = query(
-    collection(database, Collections.PRODUCTS),
-    where('category', '==', category)
+export const getProducts = async (): Promise<ProductsType[]> => {
+  const querySnapshot = await getDocs(
+    collection(database, Collections.PRODUCTS)
   )
-  const option = category !== null ? filteredProducts : allProducts
-  const querySnapshot = await getDocs(option)
 
   return querySnapshot.docs.map(
     (doc) => ({ id: doc.id, ...doc.data() } as ProductsType)
   )
 }
 
-export const useProducts = (
-  category?: string | null
-): UseQueryResult<ProductsType[]> =>
-  useQuery([PRODUCT_KEY], () => getProducts(category))
+export const useProducts = (): UseQueryResult<ProductsType[]> =>
+  useQuery([PRODUCT_KEY], () => getProducts())
--- frontend/src/screens-content/e-shop/components/product-list/product-list.tsx
+++ frontend/src/screens-content/e-shop/components/product-list/product-list.tsx
@@ -1,57 +1,51 @@
-import { ProductsType, useProducts } from 'common/api/use-products'
-import React, { useEffect, useState } from 'react'
-import ProductSkeleton from '../product-skeleton/product-skeleton'
+import { ProductsType } from 'common/api/use-products'
+import React from 'react'
 import Product from '../product/product'
 import styles from './product-list.module.scss'
 import { useCategoriesEshop } from 'common/api/use-categories-eshop'
 import { Typography } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 import { messages } from 'messages/messages'
+import { useRouter } from 'next/router'
+import { ESHOP } from 'constants/pages/urls'
+import useFilteredProducts from './use-filtered-products'
 
 const ProductList = () => {
   const { t } = useTranslation()
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
-  const { data: products, isLoading, refetch } = useProducts(selectedCategory)
+  const router = useRouter()
+  const { kategoria } = router.query
   const { data: categories } = useCategoriesEshop()
+  const { products } = useFilteredProducts((kategoria as string) ?? null)
 
   const productList = products?.map((products: ProductsType) => (
     <Product key={products.id} product={{ ...products }} />
   ))
 
-  useEffect(() => {
-    refetch()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedCategory])
+  const handleChange = (category: string) => () =>
+    router.push({
+      pathname: '/e-shop',
+      query: { kategoria: category },
+    })
 
-  const handleChange = (category: string) => () => {
-    category === selectedCategory
-      ? setSelectedCategory(null)
-      : setSelectedCategory(category)
-  }
+  const showAllProducts = () => router.push(ESHOP)
 
   const categoriesList = categories?.map((item, index) => (
     <div
       key={index}
       onClick={handleChange(item.name)}
       className={
-        selectedCategory === item.name
-          ? styles.categoryActive
-          : styles.categoryItem
+        kategoria === item.name ? styles.categoryActive : styles.categoryItem
       }
     >
       {item.name}
     </div>
   ))
 
-  const showAllProducts = () => {
-    setSelectedCategory(null)
-  }
+  const categoryTitle = kategoria ?? t(messages.allProducts)
 
-  const categoryTitle =
-    selectedCategory === null ? t(messages.allProducts) : selectedCategory
-
-  const categoryStyle =
-    selectedCategory === null ? styles.categoryActive : styles.categoryItem
+  const categoryStyle = !!kategoria
+    ? styles.categoryItem
+    : styles.categoryActive
 
   return (
     <div className={styles.eshopContainer}>
@@ -66,7 +60,8 @@ const ProductList = () => {
           {categoryTitle}
         </Typography>
         <div className={styles.productsContainer}>
-          {isLoading ? <ProductSkeleton /> : <>{productList}</>}
+          {/* {isLoading ? <ProductSkeleton /> : <>{productList}</>} */}
+          {productList}
         </div>
       </div>
     </div>
