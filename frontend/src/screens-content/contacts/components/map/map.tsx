1a77b8f61c13f537b48c9c164fa8f85d8b92f928
Dominik Secka <secka.dominik@gmail.com>
2023-07-20 12:46:20 +0200

    #207 [contacts] feat: `map` component declaration update

--- frontend/src/screens-content/contacts/components/map/map.tsx
+++ frontend/src/screens-content/contacts/components/map/map.tsx
@@ -1,10 +1,8 @@
 import styles from "../../contacts.module.scss"
 import {MAP_SOURCE} from "./constants";
 
-const Map = (): JSX.Element => {
+export const Map = () => {
   return (
     <iframe src={MAP_SOURCE} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade" className={styles.googleMap}></iframe>
   );
 };
-
-export default Map;
--- frontend/src/screens-content/contacts/contacts-layout.tsx
+++ frontend/src/screens-content/contacts/contacts-layout.tsx
@@ -1,16 +1,17 @@
-import { Container, Grid} from "@mui/material";
+import {Grid} from "@mui/material";
 import Information from "./components/information/information"
 import styles from "./contacts.module.scss"
-import Map from "./components/map/map"
+import {Map} from "./components/map/map"
+
 const ContactsLayout = (): JSX.Element => {
   return (
     <Grid container spacing={6} className={styles.mainContainer} maxWidth="lg" alignItems="flex-start">
       <Grid item xs={12} md={5}>
-          <Information/>
+        <Information/>
       </Grid>
 
       <Grid item xs={12} md={7} className={styles.mapContainer}>
-          <Map/>
+        <Map/>
       </Grid>
     </Grid>
   )
