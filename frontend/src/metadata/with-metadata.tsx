e09c478ba63cb8f3ea27e8898b44dd45e21383a0
Dominik Secka <secka.dominik@gmail.com>
2023-07-20 18:13:30 +0200

    #247 [with-metadata] fix: static page build fixed

--- frontend/src/metadata/with-metadata.tsx
+++ frontend/src/metadata/with-metadata.tsx
@@ -1,15 +1,22 @@
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import {HigherOrderComponent} from "../types/higher-order-component";
 import {Head} from "./head";
 import {metadata} from "../localization/metadata";
-import { Pages } from "constants/pages/urls";
+import {Pages} from "constants/pages/urls";
+import {localizationKey} from "../localization/localization-key";
 
 export const withMetadata: HigherOrderComponent = (WrappedComponent) =>
   // eslint-disable-next-line react/display-name
   (props) => {
     const router = useRouter()
 
-    const {titleKey, descriptionKey} = metadata[router.pathname as Pages]
+    const {
+      titleKey,
+      descriptionKey
+    } = metadata[router.pathname.replace('/sk/', '/') as Pages] ?? {
+      titleKey: localizationKey.metaTitleDefault,
+      descriptionKey: localizationKey.metaDescriptionDefault
+    }
 
     return <>
       <Head title={titleKey} description={descriptionKey}/>
