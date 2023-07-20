198789058957f86c140301bb2967bb4203cd662e
Dominik Secka <secka.dominik@gmail.com>
2023-07-20 12:31:10 +0200

    #207 [map] feat: extraction for map source out of HTML declaration

--- frontend/src/screens-content/contacts/components/map/map.tsx
+++ frontend/src/screens-content/contacts/components/map/map.tsx
@@ -1,9 +1,9 @@
-import { Container } from "@mui/material";
 import styles from "../../contacts.module.scss"
+import {MAP_SOURCE} from "./constants";
 
 const Map = (): JSX.Element => {
   return (
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d20964.90383037062!2d20.570601137573224!3d48.9418129193509!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473e48d648219f07%3A0xd2785923a1999842!2sPROGRUP%20SK%2C%20s.r.o.!5e0!3m2!1ssk!2ssk!4v1688720904220!5m2!1ssk!2ssk" allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade" className={styles.googleMap}></iframe>
+    <iframe src={MAP_SOURCE} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade" className={styles.googleMap}></iframe>
   );
 };
 
