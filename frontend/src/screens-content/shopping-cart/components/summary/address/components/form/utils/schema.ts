58a087406e0b28e50b2839de18a775526e8e2790
DavidBednarik <davidbednarik07@gmail.com>
2023-07-24 13:05:52 +0200

    regex phone number fix

--- frontend/src/screens-content/shopping-cart/components/summary/address/components/form/utils/schema.ts
+++ frontend/src/screens-content/shopping-cart/components/summary/address/components/form/utils/schema.ts
@@ -1,7 +1,6 @@
 import * as yup from 'yup'
 
-const phoneRegExp =
-  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+const phoneRegExp = /^([0-9]{10})|((\+421)[0-9]{9})$/
 
 export const FORM_SCHEMA = yup
   .object({
