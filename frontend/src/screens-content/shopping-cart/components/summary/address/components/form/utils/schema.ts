ea04e166e07e6ffaa6d8504ad5b14d8c299d789b
DavidBednarik <davidbednarik07@gmail.com>
2023-07-24 11:41:15 +0200

    regex phone

--- frontend/src/screens-content/shopping-cart/components/summary/address/components/form/utils/schema.ts
+++ frontend/src/screens-content/shopping-cart/components/summary/address/components/form/utils/schema.ts
@@ -1,5 +1,8 @@
 import * as yup from 'yup'
 
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
 export const FORM_SCHEMA = yup
   .object({
     firstName: yup.string().required('firstNameValidation'),
@@ -7,7 +10,10 @@ export const FORM_SCHEMA = yup
     address: yup.string().required('addressValidation'),
     city: yup.string().required('cityValidation'),
     postalCode: yup.string().required('postalCodeValidation'),
-    phone: yup.string().required('phoneValidation'),
+    phone: yup
+      .string()
+      .matches(phoneRegExp, 'Telefon nemá správný formát.')
+      .required('phoneValidation'),
     email: yup.string().email().required('emailValidation'),
     delivery: yup.string().required('deliveryValidation'),
     payment: yup.string().required('paymentValidation'),
