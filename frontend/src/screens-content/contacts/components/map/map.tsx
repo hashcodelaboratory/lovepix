2b63924353aceb1e67b53ffb63b33429c1713826
hydro63 <peter.hudak.63nn@gmail.com>
2023-07-26 14:24:08 +0200

    #341 formatting corrected

--- frontend/src/screens-content/contacts/components/map/map.tsx
+++ frontend/src/screens-content/contacts/components/map/map.tsx
@@ -1,7 +1,6 @@
 import styles from "../../contacts.module.scss"
 import {MAP_SOURCE} from "./constants";
 
-
 export const Map = () => {
   return (
     <iframe src={MAP_SOURCE} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade" className={styles.googleMap}></iframe>
