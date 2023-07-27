0c12e0cd5c0cc19d5d72ea1c37fce1200788f645
DavidBednarik <davidbednarik07@gmail.com>
2023-07-27 13:48:19 +0200

    product detail meta test

--- frontend/pages/product/[productID].tsx
+++ frontend/pages/product/[productID].tsx
@@ -1,29 +1,42 @@
 import ResponsiveAppBar from 'app-bar/responsive-app-bar'
-import {GetStaticPaths, GetStaticProps, NextPage} from 'next'
-import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React from 'react'
 import ProductDetailLayout from 'screens-content/e-shop/components/product-detail/product-detail'
 import FooterLayout from 'screens-content/footer/footer'
 import styles from '../../styles/Home.module.css'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useProduct } from 'common/api/use-product'
 
 const ProductDetail: NextPage = () => {
+  const router = useRouter()
+  const id = router.query.productID as string
+  const { data } = useProduct(id)
+
+  console.log(id)
+
   return (
     <div className={styles.container}>
+      <Head>
+        <title>{data?.title}</title>
+        <meta name={id} content={id} />
+      </Head>
       <header>
-        <ResponsiveAppBar/>
+        <ResponsiveAppBar />
       </header>
       <main className={styles.main}>
-        <ProductDetailLayout/>
+        <ProductDetailLayout />
       </main>
       <footer>
-        <FooterLayout/>
+        <FooterLayout />
       </footer>
     </div>
   )
 }
 
 export const getStaticPaths: GetStaticPaths<{
-  travelIds: string
+  id: string
 }> = async () => {
   return {
     paths: [], //indicates that no page needs be created at build time
@@ -31,7 +44,7 @@ export const getStaticPaths: GetStaticPaths<{
   }
 }
 
-export const getStaticProps: GetStaticProps = async ({locale}) => {
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
     props: {
       ...(await serverSideTranslations(locale ?? 'sk', ['common'])),
