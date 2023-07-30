02b48a0f21fa6fda0880966b671092d6734d6aa4
Dominik Secka <secka.dominik@gmail.com>
2023-07-30 22:51:04 +0200

    #201 [next-18next] feat: enable only `sk` localization, written comment about internationalization support for future

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -3,7 +3,9 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'sk',
-    locales: ['sk','en'],
+    locales: ['sk'],
+    // TODO: enable in time of internationalization will be required
+    // locales: ['sk','en'],
   },
   localePath: path.resolve('./public/locales'),
 }
