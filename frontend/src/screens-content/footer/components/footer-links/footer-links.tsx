64da5b1cedd7127378b63123d5e6f3699ea1b073
Samuel Gregor Hudák <hudak.samuel.gregor@gmail.com>
2023-08-02 10:42:22 +0200

    #329-FE:reworked footer links

--- frontend/src/screens-content/footer/components/footer-links/footer-links.tsx
+++ frontend/src/screens-content/footer/components/footer-links/footer-links.tsx
@@ -1,7 +1,7 @@
 import styles from "../../footer.module.scss";
 import Container from "@mui/material/Container";
 import {localizationKey} from "../../../../localization/localization-key";
-import {useCategories} from "../../../../common/api/use-categories";
+import {CategoryType, useCategories} from "../../../../common/api/use-categories";
 import {FOR_PARTNERS} from "../../../../constants/pages/titles";
 import {Link as MUILink} from "@mui/material";
 import {useTranslation} from "next-i18next";
@@ -9,87 +9,88 @@ import React from "react";
 import Link from "next/link";
 import {Pages} from "../../../../constants/pages/urls";
 import {composeUrlWithQuery} from "./util";
+import { TFunction } from 'react-i18next';
 
 type Link = {
   label: string;
   href: string | undefined;
 }
 
+const getServiceLinks = (t: TFunction<"translation", undefined>): Link[] => [{
+  label: localizationKey.ourContacts,
+  href: t(Pages.CONTACT)
+}, {
+  label: localizationKey.satisfaction,
+  // TODO: TBD
+  href: undefined
+}, {
+  label: localizationKey.possibilities,
+  // TODO: TBD
+  href: undefined
+}, {
+  label: localizationKey.creatingTime,
+  // TODO: TBD
+  href: undefined
+}, {
+  label: localizationKey.complaint,
+  // TODO: TBD
+  href: undefined
+}]
+
+const getFromPhotoLinks = (t: TFunction<"translation", undefined>): Link[] => [{
+  label: localizationKey.canvasPhoto,
+  // TODO: TBD
+  href: undefined
+}, {
+  label: localizationKey.acrylPhoto,
+  // TODO: TBD
+  href: undefined
+}, {
+  label: localizationKey.dibondPhoto,
+  // TODO: TBD
+  href: undefined
+}]
+
+const getLovePixLinks = (t: TFunction<"translation", undefined>): Link[] => [{
+  label: localizationKey.materials,
+  href: t(Pages.MATERIALS),
+}, {
+  label: localizationKey.story,
+  href: t(Pages.ABOUT_US)
+}, {
+  label: FOR_PARTNERS,
+  href: t(Pages.FOR_PARTNERS)
+}, {
+  label: localizationKey.download,
+  // TODO: TBD
+  href: undefined
+}, {
+  label: localizationKey.blog,
+  // TODO: TBD
+  href: undefined
+}]
+
+const getGalleryLinks = (t: TFunction<"translation", undefined>,categories: CategoryType[] | undefined): Link[] => (categories ?? []).map(({name}) => ({
+  label: name,
+  href: composeUrlWithQuery(t(Pages.GALLERY), {category: name})
+}))
+
 const FooterLinks = (): JSX.Element => {
   const {data: categories} = useCategories();
   const {t} = useTranslation()
 
-  const serviceLinks: Link[] = [{
-    label: localizationKey.ourContacts,
-    href: t(Pages.CONTACT)
-  }, {
-    label: localizationKey.satisfaction,
-    // TODO: TBD
-    href: undefined
-  }, {
-    label: localizationKey.possibilities,
-    // TODO: TBD
-    href: undefined
-  }, {
-    label: localizationKey.creatingTime,
-    // TODO: TBD
-    href: undefined
-  }, {
-    label: localizationKey.complaint,
-    // TODO: TBD
-    href: undefined
-  }]
-  
-  const fromPhotoLinks: Link[] = [{
-    label: localizationKey.canvasPhoto,
-    // TODO: TBD
-    href: undefined
-  }, {
-    label: localizationKey.acrylPhoto,
-    // TODO: TBD
-    href: undefined
-  }, {
-    label: localizationKey.dibondPhoto,
-    // TODO: TBD
-    href: undefined
-  }]
-  
-  const lovePixLinks: Link[] = [{
-    label: localizationKey.materials,
-    href: t(Pages.MATERIALS),
-  }, {
-    label: localizationKey.story,
-    href: t(Pages.ABOUT_US)
-  }, {
-    label: FOR_PARTNERS,
-    href: t(Pages.FOR_PARTNERS)
-  }, {
-    label: localizationKey.download,
-    // TODO: TBD
-    href: undefined
-  }, {
-    label: localizationKey.blog,
-    // TODO: TBD
-    href: undefined
-  }]
-
-  const galleryLinks: Link[] = (categories ?? []).map(({name}) => ({
-    label: name,
-    href: composeUrlWithQuery(t(Pages.GALLERY), {category: name})
-  }))
-
   const footerColumns: { title: string, links: Link[] }[] = [{
     title: localizationKey.service,
-    links: serviceLinks,
+    links: getServiceLinks(t),
   }, {
     title: localizationKey.fromPhoto,
-    links: fromPhotoLinks
+    links: getFromPhotoLinks(t)
   }, {
     title: localizationKey.gallery,
-    links: galleryLinks
+    links: getGalleryLinks(t,categories)
   }, {
     title: "Lovepix", // TODO: extract this to one general place
-    links: lovePixLinks
+    links: getLovePixLinks(t)
   }]
 
   const columns = footerColumns.map(({title, links}) => <FooterColumn
