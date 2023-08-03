804cbb59651447f4a53d7cf64369afce1695808a
Samuel Gregor Hudák <hudak.samuel.gregor@gmail.com>
2023-08-03 12:54:04 +0200

    #329-FE: add english to rewrite

---
+++ frontend/rewrites-languages/rewrites-sk.js
@@ -0,0 +1,63 @@
+const rewrites_sk = [
+    {
+        source: '/galeria',
+        destination: '/gallery',
+    },
+    {
+        source: '/materialy',
+        destination: '/materials',
+    },
+    {
+        source: '/o-nas',
+        destination: '/about-us',
+    },
+    {
+        source: '/partneri',
+        destination: '/for-partners',
+    },
+    {
+        source: '/nakupny-kosik',
+        destination: '/shopping-cart',
+    },
+    {
+        source: '/konfigurator',
+        destination: '/configurator',
+    },
+    {
+        source: '/obchod',
+        destination: '/e-shop',
+    },
+    {
+        source: '/dakujeme',
+        destination: '/thanks',
+    },
+    {
+        source: '/kontakt',
+        destination: '/contact',
+    },
+    {
+        source: '/cookies',
+        destination: '/cookies',
+    },
+    {
+        source: '/zasady-ochrany-osobnych-udajov',
+        destination: '/privacy-policy',
+    },
+    {
+        source: '/vseobecne-obchodne-podmienky',
+        destination: '/conditions',
+    },
+    {
+        source: '/profil',
+        destination: '/profile',
+    },
+    {
+        source: '/ucet',
+        destination: '/account',
+    },
+    {
+        source: '/recenzie',
+        destination: '/reviews',
+    },
+]
+module.exports = rewrites_sk
--- frontend/rewrites-next.config.js
+++ frontend/rewrites-next.config.js
@@ -1,66 +1,10 @@
+const rewrites_en = require("./rewrites-languages/rewrites-en")
+const rewrites_sk = require("./rewrites-languages/rewrites-sk")
+
+const rewrites = rewrites_sk.concat(rewrites_en)
+
 module.exports = {
-    async rewrites() {
-        return [ //For link translation to work they have to be changed in localization file and rewrites respectively
-          {
-            source: '/galeria',
-            destination: '/gallery',
-          },
-          {
-            source: '/materialy',
-            destination: '/materials',
-          },
-          {
-            source: '/o-nas',
-            destination: '/about-us',
-          },
-          {
-            source: '/partneri',
-            destination: '/for-partners',
-          },
-          {
-            source: '/nakupny-kosik',
-            destination: '/shopping-cart',
-          },
-          {
-            source: '/konfigurator',
-            destination: '/configurator',
-          },
-          {
-            source: '/obchod',
-            destination: '/e-shop',
-          },
-          {
-            source: '/dakujeme',
-            destination: '/thanks',
-          },
-          {
-            source: '/kontakt',
-            destination: '/contact',
-          },
-          {
-            source: '/cookies',
-            destination: '/cookies',
-          },
-          {
-            source: '/zasady-ochrany-osobnych-udajov',
-            destination: '/privacy-policy',
-          },
-          {
-            source: '/vseobecne-obchodne-podmienky',
-            destination: '/conditions',
-          },
-          {
-            source: '/profil',
-            destination: '/profile',
-          },
-          {
-            source: '/ucet',
-            destination: '/account',
-          },
-          {
-            source: '/recenzie',
-            destination: '/reviews',
-          },
-        ]
-    },
+  async rewrites() {
+    return rewrites //For link translation to work they have to be changed in localization files and rewrites respectively
+  }
 }
