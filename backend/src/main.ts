0b81d20a0e3e90423935143906b76eba89346d4b
MatejCarnansky <matej.carnansky@gmail.com>
2023-08-08 10:19:45 +0200

    created setup for SWAGGER UI - Backend Lovepix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from "body-parser";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:4000'
+    ],
+    //methods: ["GET", "POST"],
+    //credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+  .setTitle('Lovepix Backend documentation')
+  .setDescription('The LOVEPIX API description')
+  .setVersion('1.0')
+  .addTag('lovepix')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api-lovepix', app, document);
+
   app.use(bodyParser.json({ limit: "50mb" }));
   app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
   app.enableCors();
