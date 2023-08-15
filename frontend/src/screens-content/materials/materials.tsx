1348220af14d3a0e342d54957cb59f2dbd8866b2
hydro63 <peter.hudak.63nn@gmail.com>
2023-08-15 10:28:47 +0200

    pulled out id to constants

--- frontend/src/screens-content/materials/materials.tsx
+++ frontend/src/screens-content/materials/materials.tsx
@@ -1,6 +1,7 @@
 import { Material, materialSection } from './components/section'
 import { useTranslations } from '../../common/translations/useTranslations'
 import { useTranslation } from 'next-i18next'
+import { materialSectionId } from 'common/types/url-id'
 
 const CustomMaterials = () => {
   const { i18n } = useTranslation()
@@ -12,7 +13,7 @@ const CustomMaterials = () => {
       title: translations?.materialsCanvasTitle,
       subtitle: translations?.materialsCanvasSubTitle,
       description: translations?.materialsCanvasDescription,
-      id: "canvas",
+      id: materialSectionId.canvas,
       illustration:
         'https://firebasestorage.googleapis.com/v0/b/waller-development.appspot.com/o/materials-page%2Ffoto-na-platno.jpeg?alt=media&token=4cd5ef22-a11c-41ed-9a78-b24d1dc54ed7',
     },
@@ -20,7 +21,7 @@ const CustomMaterials = () => {
       title: translations?.materialsAcrylTitle,
       subtitle: translations?.materialsAcrylSubTitle,
       description: translations?.materialsAcrylDescription,
-      id: "acryl",
+      id: materialSectionId.acryl,
       illustration:
         'https://firebasestorage.googleapis.com/v0/b/waller-development.appspot.com/o/materials-page%2Fakryl.png?alt=media&token=a22ad0a2-dd7b-497e-834b-87b229c5b02c',
     },
@@ -28,7 +29,7 @@ const CustomMaterials = () => {
       title: translations?.materialsAluminiumTitle,
       subtitle: translations?.materialsAluminiumSubTitle,
       description: translations?.materialsAluminiumDescription,
-      id: "dibond",
+      id: materialSectionId.dibond,
       illustration:
         'https://firebasestorage.googleapis.com/v0/b/waller-development.appspot.com/o/materials-page%2Faluminium.png?alt=media&token=e7731043-dd86-4202-b048-8fd6966e0084',
     },
