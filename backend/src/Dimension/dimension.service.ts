62482643c78a054a10d86dbbbcabd9bb78abc583
Dominik Secka <secka.dominik@gmail.com>
2023-08-16 13:22:33 +0200

    #394 [dimension-service] feat: code sugar and structures

--- backend/src/Dimension/dimension.service.ts
+++ backend/src/Dimension/dimension.service.ts
@@ -1,63 +1,64 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../prisma/prisma.service";
-import { DimensionDto } from "./dto/dimension.dto";
-import { findById } from "../utils/query";
+import {Injectable} from "@nestjs/common";
+import {PrismaService} from "../prisma/prisma.service";
+import {DimensionDto} from "./dto/dimension.dto";
+import {findById} from "../utils/query";
 
 const findAllGalleriesQueryWithThatDimension = (id: string) => ({
-    where: {
-        dimensions: {
-            some: {
-                id: id
-            }
-        }
+  where: {
+    dimensions: {
+      some: {
+        id
+      }
     }
+  }
 })
 
 const updateRelationsQueryOnDimensionDelete = (id: string, dimensionIds: string[]) => ({
-    dimensionIds: {
-        set: dimensionIds.filter((dimension) => dimension !== id)
-    }
+  dimensionIds: {
+    set: dimensionIds.filter((dimensionId) => dimensionId !== id)
+  }
 })
 
 @Injectable()
 export class DimensionService {
-    constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {
+  }
 
-    async create(createData: DimensionDto) {
-        return await this.prismaService.dimension.create({
-            data: createData
-        })
-    }
+  async create(createData: DimensionDto) {
+    return this.prismaService.dimension.create({
+      data: createData
+    })
+  }
 
-    findAll() {
-        return this.prismaService.dimension.findMany();
-    }
+  findAll() {
+    return this.prismaService.dimension.findMany();
+  }
 
-    async findOne(id: string) {
-        return await this.prismaService.dimension.findUnique(findById(id));
-    }
+  async findOne(id: string) {
+    return this.prismaService.dimension.findUnique(findById(id));
+  }
 
-    async update(id: string, updateData: Partial<DimensionDto>) {
-        return await this.prismaService.dimension.update({
-            ...findById(id),
-            data: updateData
-        });
-    }
+  async update(id: string, updateData: Partial<DimensionDto>) {
+    return this.prismaService.dimension.update({
+      ...findById(id),
+      data: updateData
+    });
+  }
 
-    async remove(id: string) {
-        const galleries = await this.prismaService.gallery.findMany(findAllGalleriesQueryWithThatDimension(id));
-        
-        await this.prismaService.$transaction([
-            ...galleries.map((gallery) => this.prismaService.gallery.update({
-                ...findById(gallery.id),
-                data: updateRelationsQueryOnDimensionDelete(id, gallery.dimensionIds)
-            }))
-        ])
+  async remove(id: string) {
+    const galleries = await this.prismaService.gallery.findMany(findAllGalleriesQueryWithThatDimension(id));
 
-        return this.prismaService.dimension.delete(findById(id));
-    }
+    await this.prismaService.$transaction([
+      ...galleries.map((gallery) => this.prismaService.gallery.update({
+        ...findById(gallery.id),
+        data: updateRelationsQueryOnDimensionDelete(id, gallery.dimensionIds)
+      }))
+    ])
 
-    async removeAll() {
-        return await this.prismaService.dimension.deleteMany();
-    }
+    return this.prismaService.dimension.delete(findById(id));
+  }
+
+  async removeAll() {
+    return this.prismaService.dimension.deleteMany();
+  }
 }
