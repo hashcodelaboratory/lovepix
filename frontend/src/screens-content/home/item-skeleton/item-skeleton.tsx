9f2f245e06ac039c5c7bcc2acf47da42b344adb7
Zuzana Hrklova <xhrklo00@vutbr.cz>
2023-08-22 13:39:01 +0200

    prettier applied

--- frontend/src/screens-content/home/item-skeleton/item-skeleton.tsx
+++ frontend/src/screens-content/home/item-skeleton/item-skeleton.tsx
@@ -5,19 +5,24 @@ import styles from './item-skeleton.module.scss'
 const ItemSkeleton = () => {
   return (
     <div className={styles.itemSkeletonContainer}>
-      <Skeleton className={styles.image} sx={{ height: 300, width: 300}} animation='wave' variant='rectangular' />
+      <Skeleton
+        className={styles.image}
+        sx={{ height: 300, width: 300 }}
+        animation='wave'
+        variant='rectangular'
+      />
       <div className={styles.description}>
         <Skeleton
-            animation='wave'
-            height={50}
-            width='80%'
-            className={styles.title}
+          animation='wave'
+          height={50}
+          width='80%'
+          className={styles.title}
         />
         <Skeleton
-            animation='wave'
-            height={20}
-            width='40%'
-            className={styles.price}
+          animation='wave'
+          height={20}
+          width='40%'
+          className={styles.price}
         />
       </div>
     </div>
