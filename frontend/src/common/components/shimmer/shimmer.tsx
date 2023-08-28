64860876b125b904c225a7163dc64cfe5ce590a5
DavidBednarik <davidbednarik07@gmail.com>
2023-08-28 15:25:12 +0200

    fix resposivity on home page

--- frontend/src/common/components/shimmer/shimmer.tsx
+++ frontend/src/common/components/shimmer/shimmer.tsx
@@ -37,7 +37,7 @@ const Shimmer: FC<ShimmerProps> = ({
     )
   }
 
-  return <>{children}</>
+  return <div style={{ display: 'flex', overflow: 'auto' }}>{children}</div>
 }
 
 export default Shimmer
