93829f409253e0e0223225bedb31c071aa07ccb9
Jakub Vaňo <xvanoj00@vutbr.cz>
2023-08-30 11:38:00 +0200

    user endpoints

--- backend/src/User/user.controller.ts
+++ backend/src/User/user.controller.ts
@@ -1,61 +1,69 @@
-import { UseGuards, Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  UseGuards,
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './dto/user.dto';
 import { PartialType } from '@nestjs/mapped-types';
 import {
-    ApiBadRequestResponse,
-    ApiCreatedResponse,
-    ApiTags,
-    ApiSecurity
-  } from '@nestjs/swagger';
-import {ApikeyAuthGuard} from "./../auth/guard/apikey-auth.guard";
-import {AppSettings} from "./../constants/constants";
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiTags,
+  ApiSecurity
+} from '@nestjs/swagger';
+import { ApikeyAuthGuard } from './../auth/guard/apikey-auth.guard';
+import { AppSettings } from './../constants/constants';
 
 @ApiTags(AppSettings.USER)
 @UseGuards(ApikeyAuthGuard)
 @ApiSecurity(AppSettings.API)
 @Controller('users')
 export class UserController {
-    constructor(private readonly userService: UserService) {
-    }
+  constructor(private readonly userService: UserService) {}
 
-    @Post()
-    create(@Body() createData: UserDto) {
-        return this.userService.create(createData);
-    }
+  @Post()
+  create(@Body() createData: UserDto) {
+    return this.userService.create(createData);
+  }
 
-    @Post('many')
-    createMany(@Body() createData: UserDto[]) {
-        return this.userService.createMany(createData);
-    }
+  @Post('many')
+  createMany(@Body() createData: UserDto[]) {
+    return this.userService.createMany(createData);
+  }
 
-    @Get()
-    findAll() {
-        return this.userService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.userService.findOne(id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateData: Partial<UserDto>) {
-        return this.userService.update(id, updateData);
-    }
+  @Get(':id/orders')
+  findOrders(@Param('id') id: string) {
+    return this.userService.findOrders(id);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.userService.remove(id);
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateData: Partial<UserDto>) {
+    return this.userService.update(id, updateData);
+  }
 
-    @Get(':id/orders')
-    findOrders(@Param('id') id: string) {
-        return this.userService.findOrders(id);
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(id);
+  }
 
-    @Delete()
-    removeAll() {
-        return this.userService.removeAll();
-    }
+  @Delete()
+  removeAll() {
+    return this.userService.removeAll();
+  }
 }
