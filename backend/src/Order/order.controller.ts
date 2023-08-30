9dabe3c61215ce0a259892e110e13d3804cef797
Jakub Vaňo <xvanoj00@vutbr.cz>
2023-08-30 12:49:52 +0200

    order get price

--- backend/src/Order/order.controller.ts
+++ backend/src/Order/order.controller.ts
@@ -1,57 +1,69 @@
-import { UseGuards, Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  UseGuards,
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete
+} from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderDto } from './dto/order.dto';
 import { PartialType } from '@nestjs/mapped-types';
 import {
-    ApiBadRequestResponse,
-    ApiCreatedResponse,
-    ApiTags,
-    ApiSecurity
-  } from '@nestjs/swagger';
-import {ApikeyAuthGuard} from "./../auth/guard/apikey-auth.guard";
-import {AppSettings} from "./../constants/constants";
-
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiTags,
+  ApiSecurity
+} from '@nestjs/swagger';
+import { ApikeyAuthGuard } from './../auth/guard/apikey-auth.guard';
+import { AppSettings } from './../constants/constants';
 
 @ApiTags(AppSettings.ORDER)
 @UseGuards(ApikeyAuthGuard)
 @ApiSecurity(AppSettings.API)
 @Controller('orders')
 export class OrderController {
-    constructor(private readonly orderService: OrderService) {
-    }
+  constructor(private readonly orderService: OrderService) {}
 
-    @Post()
-    create(@Body() createData: OrderDto) {
-        return this.orderService.create(createData);
-    }
+  @Post()
+  create(@Body() createData: OrderDto) {
+    return this.orderService.create(createData);
+  }
 
-    @Post('many')
-    createMany(@Body() createData: OrderDto[]) {
-        return this.orderService.createMany(createData);
-    }
+  @Post('many')
+  createMany(@Body() createData: OrderDto[]) {
+    return this.orderService.createMany(createData);
+  }
 
-    @Get()
-    findAll() {
-        return this.orderService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.orderService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.orderService.findOne(id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.orderService.findOne(id);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() UpdateData: Partial<OrderDto>) {
-        return this.orderService.update(id, UpdateData);
-    }
+  @Get(':id/price')
+  getPrice(@Param('id') id: string) {
+    return this.orderService.getPrice(id);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.orderService.remove(id);
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() UpdateData: Partial<OrderDto>) {
+    return this.orderService.update(id, UpdateData);
+  }
 
-    @Delete()
-    removeAll() {
-        return this.orderService.removeAll();
-    }    
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.orderService.remove(id);
+  }
+
+  @Delete()
+  removeAll() {
+    return this.orderService.removeAll();
+  }
 }
--- backend/src/Order/order.service.ts
+++ backend/src/Order/order.service.ts
@@ -5,6 +5,17 @@ import { BaseService } from '../base.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
 
+const findPrice = (id: string, realtionName: string) => ({
+  ...findById(id),
+  select: {
+    [realtionName]: {
+      select: {
+        price: true
+      }
+    }
+  }
+});
+
 @Injectable()
 export class OrderService extends BaseService {
   constructor(readonly prismaService: PrismaService) {
@@ -29,6 +40,35 @@ export class OrderService extends BaseService {
     });
 
   findOne = (id: string) => this.prismaService.order.findUnique(findById(id));
+
+  getPrice = async (id: string) => {
+    const order = await this.prismaService.order.findUnique({
+      ...findById(id),
+      select: {
+        orderItems: {
+          include: {
+            product: true,
+            image: true
+          }
+        },
+        shipment: true,
+        payment: true
+      }
+    });
+    let images: number[] = [];
+    let products: number[] = [];
+    order.orderItems.map((item) => {
+      if (item.productId) products.push(item.product.price * item.quantity);
+
+      if (item.imageId) images.push(item.image.price * item.quantity);
+    });
+    return (
+      images.reduce((a, b) => a + b, 0) +
+      products.reduce((a, b) => a + b, 0) +
+      order.shipment.price +
+      order.payment.price
+    );
+  };
 
   update = (id: string, data: Partial<OrderDto>) =>
     this.prismaService.order.update({
