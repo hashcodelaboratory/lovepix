94551c42419fd13dfd1daae7a605a8963a6d6c09
Michal <michaldzurilla1@gmail.com>
2023-11-06 15:27:43 +0100

    [532]: cart - voucher - expiration added

--- frontend/src/screens-content/shopping-cart/components/summary/delivery/delivery.tsx
+++ frontend/src/screens-content/shopping-cart/components/summary/delivery/delivery.tsx
@@ -1,26 +1,52 @@
-import styles from "../../../shopping-cart.module.scss";
-import { useTranslation } from "next-i18next";
-import { localizationKey } from "../../../../../localization/localization-key";
-import { FormControl, FormControlLabel, FormHelperText, Radio, RadioGroup } from "@mui/material";
-import { Delivery as DeliveryOptions } from "../../../../../common/enums/delivery";
-import { Control, Controller } from "react-hook-form";
-import { FormInputs } from "../../../../../common/types/form";
+import styles from '../../../shopping-cart.module.scss'
+import { useTranslation } from 'next-i18next'
+import { localizationKey } from '../../../../../localization/localization-key'
+import {
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  Radio,
+  RadioGroup,
+} from '@mui/material'
+import { Delivery as DeliveryOptions } from '../../../../../common/enums/delivery'
+import { Control, Controller, UseFormSetValue } from 'react-hook-form'
+import { FormInputs } from '../../../../../common/types/form'
+import { useMemo } from 'react'
 
 type DeliverySectionProps = {
-  message?: string;
-  control: Control<FormInputs>;
+  message?: string
+  control: Control<FormInputs>
+  freeDelivery?: boolean
+  setValue: UseFormSetValue<FormInputs>
 }
 
-const Delivery = ({ control, message }: DeliverySectionProps) => {
-  const { t } = useTranslation();
+const Delivery = ({
+  control,
+  message,
+  freeDelivery,
+  setValue,
+}: DeliverySectionProps) => {
+  const { t } = useTranslation()
+
+  const isFreeDeliveryAvailable = useMemo(() => {
+    if (freeDelivery) {
+      setValue('delivery', DeliveryOptions.FREE_DELIVERY)
+      return true
+    } else {
+      setValue('delivery', undefined)
+      return false
+    }
+  }, [freeDelivery])
 
   return (
     <div className={styles.cartContainer}>
       <div className={styles.cartTitleContainer}>
-        <h3 className={styles.cartTitleText}>{String(t(localizationKey.delivery))}</h3>
+        <h3 className={styles.cartTitleText}>
+          {String(t(localizationKey.delivery))}
+        </h3>
       </div>
       <Controller
-        name="delivery"
+        name='delivery'
         control={control}
         rules={{ required: true }}
         render={({ field }) => (
@@ -30,39 +56,64 @@ const Delivery = ({ control, message }: DeliverySectionProps) => {
               onChange={field.onChange}
               value={field.value}
             >
-              <FormControlLabel
-                value={DeliveryOptions.COURIER}
-                control={<Radio />}
-                label={
-                  <div className={styles.radioGroupLabel}>
-                    <p className={styles.priceBox}>5.00 €</p>
-                    <div className={styles.deliveryLightText}>{String(t(localizationKey.courier))}</div>
-                  </div>
-                }
-                className={styles.deliveryField}
-              />
-              <FormControlLabel
-                value={DeliveryOptions.PERSONAL_COLLECT}
-                control={<Radio />}
-                label={
-                  <div className={styles.radioGroupLabel}>
-                    <p className={styles.priceBox}>{String(t(localizationKey.free))}</p>
-                    <div className={styles.deliveryLightText}>{String(t(localizationKey.personalCollect))}</div>
-                  </div>
-                }
-                className={styles.deliveryField}
-              />
+              {!isFreeDeliveryAvailable && (
+                <FormControlLabel
+                  value={DeliveryOptions.COURIER}
+                  control={<Radio />}
+                  label={
+                    <div className={styles.radioGroupLabel}>
+                      <p className={styles.priceBox}>5.00 €</p>
+                      <div className={styles.deliveryLightText}>
+                        {String(t(localizationKey.courier))}
+                      </div>
+                    </div>
+                  }
+                  className={styles.deliveryField}
+                />
+              )}
+              {!isFreeDeliveryAvailable && (
+                <FormControlLabel
+                  value={DeliveryOptions.PERSONAL_COLLECT}
+                  control={<Radio />}
+                  label={
+                    <div className={styles.radioGroupLabel}>
+                      <p className={styles.priceBox}>
+                        {String(t(localizationKey.free))}
+                      </p>
+                      <div className={styles.deliveryLightText}>
+                        {String(t(localizationKey.personalCollect))}
+                      </div>
+                    </div>
+                  }
+                  className={styles.deliveryField}
+                />
+              )}
+              {isFreeDeliveryAvailable && (
+                <FormControlLabel
+                  value={DeliveryOptions.FREE_DELIVERY}
+                  control={<Radio checked />}
+                  label={
+                    <div className={styles.radioGroupLabel}>
+                      <p className={styles.priceBox}>
+                        {String(t(localizationKey.free))}
+                      </p>
+                      <div className={styles.deliveryLightText}>
+                        {'Máte nárok na dopravu zdarma'}
+                      </div>
+                    </div>
+                  }
+                  className={styles.deliveryField}
+                />
+              )}
             </RadioGroup>
             {message && (
-              <FormHelperText error>
-                {String(t(message))}
-              </FormHelperText>
+              <FormHelperText error>{String(t(message))}</FormHelperText>
             )}
           </FormControl>
         )}
       />
     </div>
-  );
-};
+  )
+}
 
-export default Delivery;
+export default Delivery
