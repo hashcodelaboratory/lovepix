f8b69e461c0a5bd98ffd3c232f547cb16ffc0a7a
Michal <michaldzurilla1@gmail.com>
2023-12-12 15:18:20 +0100

    #557 - E-shop - grid <-> flex

--- frontend/src/screens-content/home/enums/enums.ts
+++ frontend/src/screens-content/home/enums/enums.ts
@@ -1,14 +1,15 @@
 export enum ImageLayout {
-  FIXED = "fixed",
-  FILL = "fill",
-  INTRINSIC = "intrinsic"
+  FIXED = 'fixed',
+  FILL = 'fill',
+  INTRINSIC = 'intrinsic',
+  RESPONSIVE = 'responsive',
 }
 
 export enum TextAlign {
-  RIGHT = "right",
+  RIGHT = 'right',
 }
 
 export enum ObjectFit {
-  CONTAIN = "contain",
-  COVER = "cover",
+  CONTAIN = 'contain',
+  COVER = 'cover',
 }
