6e45b6443b8a23d43007c0247050526ee0a73e3c
Michal <michaldzurilla1@gmail.com>
2023-12-18 15:29:30 +0100

    #82 - profile - orders - translation added

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -3,6 +3,8 @@ import styles from '../styles/Home.module.css'
 import ResponsiveAppBar from '../src/app-bar/responsive-app-bar'
 import FooterLayout from '../src/screens-content/footer/footer'
 import ProfileLayout from '../src/screens-content/profile/profile'
+import { GetStaticProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 const Profile: NextPage = () => {
   return (
@@ -23,3 +25,11 @@ const Profile: NextPage = () => {
 }
 
 export default Profile
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? 'sk', ['common'])),
+    },
+  }
+}
