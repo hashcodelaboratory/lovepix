20ab6f08d7008d349fcee4ac015c00d2647679b5
Michal <michaldzurilla1@gmail.com>
2024-01-09 15:16:23 +0100

    profile - data fetching refactor

--- frontend/src/common/api/use-orders.ts
+++ frontend/src/common/api/use-orders.ts
@@ -1,4 +1,4 @@
-import { useQuery, UseQueryResult } from 'react-query'
+import { useQuery, UseQueryOptions, UseQueryResult } from 'react-query'
 import { Order } from '../types/order'
 
 export const ORDERS_KEY = 'ORDERS'
@@ -9,5 +9,8 @@ const getOrders = async (userId?: string | null): Promise<Order[]> => {
   return res.json()
 }
 
-export const useOrders = (userId?: string | null): UseQueryResult<Order[]> =>
-  useQuery([ORDERS_KEY], () => getOrders(userId))
+export const useOrders = (
+  userId?: string | null,
+  options?: UseQueryOptions<any, any, any, any>
+): UseQueryResult<Order[]> =>
+  useQuery([ORDERS_KEY], () => getOrders(userId), options)
