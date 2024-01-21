0aae9dd3fa10415dbe5d7c793e5874c3e57f3c47
Dominik Secka <secka.dominik@gmail.com>
2024-01-21 21:45:24 +0100

    feat: [#582] sentry-service integration tests written

--- frontend/src/analytics/sentry/sentry-service.test.ts
+++ frontend/src/analytics/sentry/sentry-service.test.ts
@@ -1,17 +1,72 @@
 import * as Sentry from '@sentry/nextjs'
 
-jest.mock('@sentry/nextj')
+jest.mock('@sentry/nextjs')
 
 describe('SentryService', () => {
+  beforeEach(jest.clearAllMocks)
+
   describe('captureException', () => {
-    const { sentryService } = jest.requireActual('./sentry-service')
+    it('should call Sentry.captureException with expected default parameters', () => {
+      const { sentryService } = jest.requireActual('./sentry-service')
 
-    sentryService.captureException(new Error('<test_exception>'))
+      sentryService.captureException(new Error('<test_exception>'))
 
-    expect(Sentry.captureException).toHave
+      expect(Sentry.captureException).toHaveBeenCalledTimes(1)
+      expect(Sentry.captureException).toHaveBeenCalledWith(
+        new Error('<test_exception>'),
+        undefined
+      )
+    })
+
+    it('should call Sentry.captureException with expected additional information', () => {
+      const { sentryService } = jest.requireActual('./sentry-service')
+
+      sentryService.captureException(new Error('<test_exception>'), {
+        user: {
+          id: '<test_user_id>',
+        },
+      })
+
+      expect(Sentry.captureException).toHaveBeenCalledTimes(1)
+      expect(Sentry.captureException).toHaveBeenCalledWith(
+        new Error('<test_exception>'),
+        {
+          user: {
+            id: '<test_user_id>',
+          },
+        }
+      )
+    })
   })
 
   describe('captureMessage', () => {
-    const { sentryService } = jest.requireActual('./sentry-service')
+    it('should call Sentry.captureMessage with expected default parameters', () => {
+      const { sentryService } = jest.requireActual('./sentry-service')
+
+      sentryService.captureMessage('<test_message>')
+
+      expect(Sentry.captureMessage).toHaveBeenCalledTimes(1)
+      expect(Sentry.captureMessage).toHaveBeenCalledWith(
+        '<test_message>',
+        undefined
+      )
+    })
+
+    it('should call Sentry.captureMessage with expected additional information', () => {
+      const { sentryService } = jest.requireActual('./sentry-service')
+
+      sentryService.captureMessage('<test_message>', {
+        user: {
+          id: '<test_user_id>',
+        },
+      })
+
+      expect(Sentry.captureMessage).toHaveBeenCalledTimes(1)
+      expect(Sentry.captureMessage).toHaveBeenCalledWith('<test_message>', {
+        user: {
+          id: '<test_user_id>',
+        },
+      })
+    })
   })
 })
